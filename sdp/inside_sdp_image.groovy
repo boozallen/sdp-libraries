8f0bf037ed6270c5137148f1ccd2ef32c22c38fb
kottoson <kjottoson@yahoo.com>
2019-01-18 10:19:08 -0500

    Update inside_sdp_image.groovy

--- sdp/inside_sdp_image.groovy
+++ sdp/inside_sdp_image.groovy
@@ -13,14 +13,18 @@
   }
 */
 void call(String img, Closure body){
-  def sdp_img_repo = pipelineConfig.sdp_image_repository ?:
-                     { error "SDP Image Repository not defined in Pipeline Config" }()
+  
+  def sdp_img_reg = config.registry ?:
+                    { error "SDP Image Registry not defined in Pipeline Config" } ()
+  
+  def sdp_img_repo = config.repository ?:
+                     { echo "SDP Image Repository not defined in Pipeline Config. Defaulting to \"sdp\""; return "sdp" }()
                      
-  def sdp_img_repo_cred = pipelineConfig.sdp_image_repository_credential ?:
+  def sdp_img_repo_cred = config.cred ?:
                           { error "SDP Image Repository Credential not defined in Pipeline Config" }()
   
   docker.withRegistry(sdp_img_repo, sdp_img_repo_cred){
-    docker.image("sdp/${img}").inside{
+    docker.image("${sdp_image_repo}/${img}").inside{
       body.resolveStrategy = Closure.DELEGATE_FIRST
       body.delegate = this
       body()
