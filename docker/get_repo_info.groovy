e270557d5723c317dbd0333ab13b1c4ff6c70d65
Carlos OKieffe <okieffe_carlos@bah.com>
2019-01-24 13:38:27 -0500

    updated error messaging

--- docker/get_repo_info.groovy
+++ docker/get_repo_info.groovy
@@ -2,9 +2,9 @@ def call() {
     def ret = [:]
     errors = []
     ret.repo = config.registry ?:
-            { errors << "Application Docker Image Registry not defined in pipeline config" }()
+            { errors << "Application Docker Image Registry, library.docker.registry, not defined in pipeline config" }()
     ret.cred = config.cred ?:
-            { errors << "Application Docker Image Repository Credential not defined in pipeline config" }()
+            { errors << "Application Docker Image Repository Credential, library.docker.cred, not defined in pipeline config" }()
 
     if(!errors.empty) {
         error errors.join("; ")
