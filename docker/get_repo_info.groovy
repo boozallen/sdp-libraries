a25922f47635b82d8f4bd903be7d051bee8444bc
Carlos OKieffe <okieffe_carlos@bah.com>
2019-01-24 16:18:12 -0500

    figuring out why errors is not changing (2); using parens

--- docker/get_repo_info.groovy
+++ docker/get_repo_info.groovy
@@ -1,10 +1,10 @@
 def call() {
     def ret = []
     errors = []
-    ret << config.registry ?:
-            { errors << "Application Docker Image Registry, libraries.docker.registry, not defined in pipeline config" }()
-    ret << config.cred ?:
-            { errors << "Application Docker Image Repository Credential, libraries.docker.cred, not defined in pipeline config" }()
+    ret << (config.registry ?:
+            { errors << "Application Docker Image Registry, libraries.docker.registry, not defined in pipeline config" }() )
+    ret << ( config.cred ?:
+            { errors << "Application Docker Image Repository Credential, libraries.docker.cred, not defined in pipeline config" }() )
 
     if(!errors.empty) {
         error errors.join("; ")
