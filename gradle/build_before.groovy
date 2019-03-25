1916e86e58137307cb3ddbc41dd77797fc237f4d
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-25 12:16:09 -0400

    initial add of gradle build_source before step

--- gradle/build_before.groovy
+++ gradle/build_before.groovy
@@ -2,6 +2,7 @@
 @BeforeStep
 void call(Map context){
     if (context.step.equals("static_code_analysis")){
+        echo "build_source before static_code_analysis"
         build_source()
     }
 }
