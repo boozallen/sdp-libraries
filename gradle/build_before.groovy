37612787311640ab55f0004d56e566fcc50f18a5
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-25 12:22:04 -0400

    updating logging statements in beforeStep

--- gradle/build_before.groovy
+++ gradle/build_before.groovy
@@ -2,7 +2,9 @@
 @BeforeStep
 void call(Map context){
     if (context.step.equals("static_code_analysis")){
-        echo "build_source before static_code_analysis"
+        println "build_source before static_code_analysis"
         build_source()
+    } else {
+        println "not static_code_analysis"
     }
 }
