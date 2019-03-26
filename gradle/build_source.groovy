e9f0df30657d9f558cc6b6ce2c30c525fea2e6e0
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-25 21:07:12 -0400

    adding actual build for gradle

---
+++ gradle/build_source.groovy
@@ -0,0 +1,11 @@
+def call(){
+    stage "Building Gradle Project", {
+        node{
+            unstash "workspace"
+
+            sh "gradle clean build"
+
+            stash "workspace"
+        }
+    }
+}
