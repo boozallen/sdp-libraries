11e84e984bc79416cf1715b476d97b61765ca76b
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-26 13:12:15 -0400

    removed else statement that simply printed that it was called

--- gradle/build_before.groovy
+++ gradle/build_before.groovy
@@ -4,7 +4,5 @@ void call(Map context){
     if (context.step.equals("static_code_analysis")){
         println "build_source before static_code_analysis"
         build_source()
-    } else {
-        println "not static_code_analysis"
     }
 }
