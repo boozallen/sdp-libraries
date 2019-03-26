360fae14dbde19c0daf67a3d968c9d4eb1fb64e9
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-25 21:20:25 -0400

    renaming method to test default step with groovy script

--- gradle/build_before.groovy
+++ gradle/build_before.groovy
@@ -3,7 +3,7 @@
 void call(Map context){
     if (context.step.equals("static_code_analysis")){
         println "build_source before static_code_analysis"
-        build_source()
+        build_source2()
     } else {
         println "not static_code_analysis"
     }
