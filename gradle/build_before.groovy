29aae4981c07f65359bdf6c5ac5672c7c484b455
Carlos OKieffe <okieffe_carlos@bah.com>
2019-03-25 21:29:41 -0400

    reverting to build_source

--- gradle/build_before.groovy
+++ gradle/build_before.groovy
@@ -3,7 +3,7 @@
 void call(Map context){
     if (context.step.equals("static_code_analysis")){
         println "build_source before static_code_analysis"
-        build_source2()
+        build_source()
     } else {
         println "not static_code_analysis"
     }
