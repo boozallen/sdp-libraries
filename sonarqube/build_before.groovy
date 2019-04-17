24f50bde922807ede051122d2fd40efea79a3f76
Carlos OKieffe <okieffe_carlos@bah.com>
2019-04-17 17:55:06 -0400

    changed sonarqube build step behavior; build_step and require_build_step

--- sonarqube/build_before.groovy
+++ sonarqube/build_before.groovy
@@ -2,10 +2,10 @@
 @BeforeStep
 void call(Map context){
     if (context.step.equals("static_code_analysis")){
-        if( config.require_build_source || getBinding().hasVariable("build_source")) {
-            if( config.require_build_source || getBinding().getVariable("build_source") instanceof org.boozallen.plugins.jte.binding.StepWrapper) {
-                println "build_source before static_code_analysis"
-                build_source()
+        if( config.require_build_step || (config.build_step && getBinding().hasVariable(config.build_step)) ) {
+            if( config.require_build_step || getBinding().getVariable(config.build_step) instanceof org.boozallen.plugins.jte.binding.StepWrapper) {
+                println "executing before static_code_analysis: step: ${config.build_step ?: 'not defined'}"
+                "${config.build_step}"()
             }
         }
     }
