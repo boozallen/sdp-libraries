caff0a14bce40cde2c31587d5ff345827e23142a
Carlos OKieffe <okieffe_carlos@bah.com>
2019-04-17 10:56:11 -0400

    fixing issue where if build_source is not implemented the build_before will cause an exception

--- sonarqube/build_before.groovy
+++ sonarqube/build_before.groovy
@@ -2,7 +2,11 @@
 @BeforeStep
 void call(Map context){
     if (context.step.equals("static_code_analysis")){
-        println "build_source before static_code_analysis"
-        build_source()
+        if( config.require_build_source || getBinding().hasVariable("build_source")) {
+            if( config.require_build_source || getBinding().getVariable("build_source") instanceof org.boozallen.plugins.jte.binding.StepWrapper) {
+                println "build_source before static_code_analysis"
+                build_source()
+            }
+        }
     }
 }
