1fdf0ff38fada80a9571a6e85d77f4ff6d3b2979
kottoson <kjo9fq@virginia.edu>
2019-04-30 15:02:08 -0400

    remove docker library from pipeline

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,11 +1,5 @@
 libraries {
   github_enterprise
-  docker {
-    build_strategy = "modules"
-    registry = "docker-registry.default.svc:5000"
-    cred = "openshift-docker-registry"
-    repo_path_prefix = "keegan-sdp"
-  }
   sdp {
     images{
       registry = "https://docker-registry.default.svc:5000"
