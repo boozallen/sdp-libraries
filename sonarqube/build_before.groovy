da773d730d292e9d9c58606a2a0d2b865b1ad066
Carlos OKieffe <okieffe_carlos@bah.com>
2019-06-04 13:20:36 -0400

    throwing exception if require_build_step and no step exists

--- sonarqube/build_before.groovy
+++ sonarqube/build_before.groovy
@@ -2,7 +2,11 @@
 @BeforeStep
 void call(Map context){
     if (context.step.equals("static_code_analysis")){
-        if( config.require_build_step || getBinding().hasStep(config.build_step) ) {
+      boolean hasStep = getBinding().hasStep(config.build_step)
+      if( config.require_build_step && !hasStep ){
+         throw new Exception("require_build_step with no defined step named: ${config.build_step}")
+      }
+      else if( hasStep ) {
             def step = getBinding().getStep(config.build_step)
             String stepMethod = config.build_step_method ?: "call"
             step."${stepMethod}"()
