780644ecf3b562bdef7b7458bd971332b32511c1
Carlos OKieffe <okieffe_carlos@bah.com>
2019-06-11 12:36:35 -0400

    adding BuildBeforeSpec and updated build_before for efficiency; added jte dependency causes 'git' to be mocked so updated DeployToSpec to remove that mock

--- sonarqube/build_before.groovy
+++ sonarqube/build_before.groovy
@@ -1,15 +1,17 @@
+import org.boozallen.plugins.jte.hooks.BeforeStep
 
 @BeforeStep
 void call(Map context){
     if (context.step.equals("static_code_analysis")){
-      boolean hasStep = getBinding().hasStep(config.build_step)
-      if( config.require_build_step && !hasStep ){
-         throw new Exception("require_build_step with no defined step named: ${config.build_step}")
+      String buildStep = config.build_step
+      boolean hasStep = getBinding().hasStep(buildStep)
+      if( !hasStep && config.require_build_step ){
+         throw new Exception("require_build_step with no defined step named: ${buildStep}")
       }
       else if( hasStep ) {
-            def step = getBinding().getStep(config.build_step)
+            def step = getBinding().getStep(buildStep)
             String stepMethod = config.build_step_method ?: "call"
             step."${stepMethod}"()
-        }
+      }
     }
 }
---
+++ sonarqube/unit-tests/BuildBeforeSpec.groovy
@@ -0,0 +1,181 @@
+/*
+  Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
+  This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
+*/
+
+package sonarqube
+
+import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
+import org.boozallen.plugins.jte.binding.TemplateBinding
+import org.boozallen.plugins.jte.binding.StepWrapper
+
+public class BuildBeforeSpec extends JenkinsPipelineSpecification {
+
+  def BuildBefore = null
+
+  public static class DummyException extends RuntimeException {
+		public DummyException(String _message) { super( _message ); }
+	}
+
+  def setup() {
+    BuildBefore = loadPipelineScriptForTest("sonarqube/build_before.groovy")
+  }
+
+  def "context.step != static_code_analysis; nothing" () {
+    setup:
+    def context = [:]
+    TemplateBinding binding = Mock(TemplateBinding)
+    0 * binding.hasStep(_)
+    0 * binding.getVariable("config")
+
+    when:
+    BuildBefore.call(context)
+    then:
+    notThrown(Exception)
+  }
+
+  def "!require_build_step && !hasStep; nothing" () {
+    setup:
+    String stepName = "stepName"
+    def context = [step:'static_code_analysis']
+    def config = [build_step:stepName]
+
+    TemplateBinding binding = Mock(TemplateBinding)
+    1 * binding.hasStep(stepName) >> { return false}
+    2 * binding.getVariable("config") >> {return config }
+    0 * binding.getStep(stepName)
+
+    BuildBefore.setBinding(binding)
+
+    when:
+    def res = BuildBefore.call(context)
+
+    then:
+    null == res
+    notThrown(Exception)
+
+  }
+
+  def "require_build_step && !hasStep; throw Exception" () {
+    setup:
+    String stepName = "stepName"
+    def context = [step:'static_code_analysis']
+    def config = [build_step:stepName, require_build_step:true]
+
+    TemplateBinding binding = Mock(TemplateBinding)
+    1 * binding.hasStep(stepName) >> { return false}
+    2 * binding.getVariable("config") >> {return config }
+    0 * binding.getStep(stepName)
+
+    BuildBefore.setBinding(binding)
+
+    when:
+    def res = BuildBefore.call(context)
+
+    then:
+    null == res
+    Exception e = thrown(Exception)
+    e.message.contains("require_build_step with no defined step named:")
+
+  }
+
+  def "hasStep && getStep; works" () {
+    setup:
+    String stepName = "stepName"
+    def context = [step:'static_code_analysis']
+    def config = [build_step:stepName]
+    StepWrapper step = Mock(StepWrapper)
+    1 * step.call(_) >> {  }
+    0 * step.methodMissing(_, _)
+
+    TemplateBinding binding = Mock(TemplateBinding)
+    1 * binding.hasStep(stepName) >> { return true  }
+    2 * binding.getVariable("config") >> {return config }
+    1 * binding.getStep(stepName) >> { return step }
+
+    BuildBefore.setBinding(binding)
+
+    when:
+    BuildBefore.call(context)
+
+    then:
+    notThrown(Exception)
+
+  }
+
+  def "hasStep && require_build_step && getStep; works" () {
+    setup:
+    String stepName = "stepName"
+    def context = [step:'static_code_analysis']
+    def config = [build_step:stepName, require_build_step:true]
+    StepWrapper step = Mock(StepWrapper)
+    1 * step.call(_) >> {  }
+    0 * step.methodMissing(_, _)
+
+    TemplateBinding binding = Mock(TemplateBinding)
+    1 * binding.hasStep(stepName) >> { return true  }
+    2 * binding.getVariable("config") >> {return config }
+    1 * binding.getStep(stepName) >> { return step }
+
+    BuildBefore.setBinding(binding)
+
+    when:
+    BuildBefore.call(context)
+
+    then:
+    notThrown(Exception)
+
+  }
+
+  def "hasStep && getStep && build_step_method; works" () {
+    setup:
+    String stepName = "stepName"
+    String stepMethod = "stepMethod"
+    def context = [step:'static_code_analysis']
+    def config = [build_step:stepName, build_step_method:stepMethod]
+    StepWrapper step = Mock(StepWrapper)
+    0 * step.call(_) >> {  }
+    1 * step.methodMissing(stepMethod, _) >> {}
+
+    TemplateBinding binding = Mock(TemplateBinding)
+    1 * binding.hasStep(stepName) >> { return true  }
+    2 * binding.getVariable("config") >> {return config }
+    1 * binding.getStep(stepName) >> { return step }
+
+    BuildBefore.setBinding(binding)
+
+    when:
+    BuildBefore.call(context)
+
+    then:
+    notThrown(Exception)
+
+  }
+
+  def "hasStep && getStep && build_step_method is wrong; fails " () {
+    setup:
+    String stepName = "stepName"
+    String stepMethod = "stepMethod"
+    def context = [step:'static_code_analysis']
+    def config = [build_step:stepName, build_step_method:stepMethod]
+    StepWrapper step = Mock(StepWrapper)
+    0 * step.call(_) >> {  }
+    1 * step.methodMissing(stepMethod, _) >> { throw Exception }
+
+    TemplateBinding binding = Mock(TemplateBinding)
+    1 * binding.hasStep(stepName) >> { return true  }
+    2 * binding.getVariable("config") >> {return config }
+    1 * binding.getStep(stepName) >> { return step }
+
+    BuildBefore.setBinding(binding)
+
+    when:
+    BuildBefore.call(context)
+
+    then:
+    Exception e = thrown()
+    e != null
+
+  }
+
+}
