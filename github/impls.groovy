d019899d11c077f8dbc40e593e5336bf8be43f74
Carlos OKieffe <okieffe_carlos@bah.com>
2019-06-25 09:35:28 -0400

    changed names of implemenation/source_types

--- github/impls.groovy
+++ github/impls.groovy
@@ -1,5 +1,8 @@
 
 def call(){
+    List options = ['github_standard', 'github_enterprise']
     String impl = config.source_type
-    return getBinding().getStep(impl)
+
+    return options.contains(impl) ? getBinding().getStep(impl) :
+            { error "github.config.source_type: ${impl} is not a valid option" } ()
 }
