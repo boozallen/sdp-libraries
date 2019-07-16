e2fb268deea328d9f75df7f6a1a9fd28ecb94e03
ottoson-keegan <ottoson_keegan@bah.com>
2019-07-16 16:47:24 -0400

    fixing typo in owasp_zap/library_config.groovy

--- owasp_zap/library_config.groovy
+++ owasp_zap/library_config.groovy
@@ -1,6 +1,6 @@
 fields{
   optional{
-    target = ~/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#\[\]@!\$&'\(\)\*\+,;=.]+$/ url
+    target = ~/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#\[\]@!\$&'\(\)\*\+,;=.]+$/
     vuln_threshold = ["Ignore", "Low", "Medium", "High", "Informational"]
   }
 }
