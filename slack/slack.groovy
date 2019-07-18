fe0058588f72dfe95300e8106d27a53152bceb17
ottoson-keegan <ottoson_keegan@bah.com>
2019-07-18 13:15:01 -0400

    updating the Slack lib to leverage lifecycle hooks

--- slack/slack.groovy
+++ slack/slack.groovy
@@ -3,8 +3,10 @@
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
 
-def call(){
-    switch(currentBuild.result){
+//Alert when job is finished
+@CleanUp
+def slack_cleanup(context){
+    switch(context.status){
         case null: // no result set yet means success
         case "SUCCESS":
           slackSend color: "good", message: "Build Successful: ${env.JOB_URL}"
@@ -15,4 +17,12 @@ def call(){
         default:
           echo "Slack Notifier doing nothing: ${currentBuild.result}"
     }
+}
+
+//Alert on successful deployment
+@AfterStep
+def slack_report_deployment(context){
+  if (context.step == "deploy_to" && context.status == "SUCCESS") {
+    slackSend color: '#0000ff', message: "Deployment Successful: ${env.JOB_URL}"
+  }
 }
