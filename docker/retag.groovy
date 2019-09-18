21f6dab13bb1d75c405ff90693265680f3821264
aleckeller <alec.keller.17@gmail.com>
2019-09-18 08:41:22 -0400

    Update retag.groovy

--- docker/retag.groovy
+++ docker/retag.groovy
@@ -10,9 +10,9 @@ void call(old_tag, new_tag){
         login_to_registry()
 
         get_images_to_build().each{ img ->
-          sh "docker pull ${img.registry}/${img.repo}:${old_tag}"
-          sh "docker tag ${img.registry}/${img.repo}:${old_tag} ${img.registry}/${img.repo}:${new_tag}"
-          sh "docker push ${img.registry}/${img.repo}:${new_tag}"
+          sh "docker pull ${img.registry}/${img.repo.toLowerCase()}:${old_tag}"
+          sh "docker tag ${img.registry}/${img.repo.toLowerCase()}:${old_tag} ${img.registry}/${img.repo.toLowerCase()}:${new_tag}"
+          sh "docker push ${img.registry}/${img.repo.toLowerCase()}:${new_tag}"
         }
     }
 }
