94edf308f2587a26eb194fb2e0e1de6e103118b8
Carlos OKieffe <okieffe_carlos@bah.com>
2019-09-25 16:25:23 -0400

    initial add of an experimental library

---
+++ python/unit_test.groovy
@@ -0,0 +1,29 @@
+void call(){
+  stage(config.stage_name ?: "Python Unit test"){
+    def image = config.docker_image ?: "python:3.7.4"
+    docker.image(image).inside{
+      def test_results_file = config.test_report_path ?: "junit.xml"
+
+      unstash "workspace"
+      echo "running python unit tests"
+
+      try {
+        String testInstalls = config.test_installs ?: "pip install beautifulsoup4 lxml dateutils nltk pytest"
+        String testCmd = config.test_cmd ?: "python -m pytest --junitxml=junit.xml"
+        sh "${testInstalls} && ${testCmd}"
+        stash name: "workspace",
+                includes: config.stash?.includes ?: "**",
+                excludes: config.stash?.excludes ?: "node_modules/**",
+                useDefaultExcludes: false,
+                allowEmpty: true
+
+        echo "archiving ${test_results_file}"
+
+        archiveArtifacts allowEmptyArchive: true, artifacts: "${test_results_file}"
+      }catch(any){
+        println "issue with python unit tests"
+        unstable("issue with python unit tests")
+      }
+    }
+  }
+}
