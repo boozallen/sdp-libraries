e81c0ec3514a6b6ac3b54db0cfb7d5deb6bfbaf3
Carlos OKieffe <okieffe_carlos@bah.com>
2019-10-01 22:30:08 -0400

    adding configurable build_script and build_install

--- npm/build.groovy
+++ npm/build.groovy
@@ -1,9 +1,21 @@
 void call(){
   stage("build"){
-    def nodeImage = config.nodeImage ?: "node:10.16.0-stretch-slim"
+    def nodeImage = config.node_image ?: "node:latest"
+    def buildScript = config.build_script ?: "build"
+    def installScript = config.build_install ?: "install"
+
+    if(fileExists("package.json")){
+      def pkg = readJSON("package.json")
+      if(!pkg.scripts[buildScript]){
+        error "cannot execute 'npm run ${buildScript}'.  '${buildScript}' not found in scripts block of package.json file"
+      }
+    }else{
+      error "no package.json file found"
+    }
+
     docker.image(nodeImage).inside{
       unstash "workspace"
-      sh "npm install && npm run build"
+      sh "${installScript ? 'npm ' + installScript + ' && ' : ''}npm run ${buildScript}"
       stash name: "workspace",
         includes: config.stash?.includes ?: "**",
         excludes: config.stash?.excludes ?: "node_modules/**",
