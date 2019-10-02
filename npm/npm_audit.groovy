fc30347ea5e1f7934d85c5f7b39975d54997e1b0
Carlos OKieffe <okieffe_carlos@bah.com>
2019-10-02 13:15:44 -0400

    changed to config.node_image

--- npm/npm_audit.groovy
+++ npm/npm_audit.groovy
@@ -3,21 +3,21 @@ void call(){
     stage('NPM Package Analysis') {
       unstash "workspace"
         // npm_base: path from the root of your repository to the NPM project directory
-        def npm_base = (config.npm_base instanceof String) ? config.npm_base : ""
+        def npm_base = config.npm_base ?: ""
 
         // use_npm_default_registry:  if the audit will use the default npm registry (https://registry.npmjs.org) or the project's registry,
         def use_default_registry = (config.use_npm_default_registry instanceof Boolean) ? config.use_npm_default_registry : false
 
         def project_base = "${WORKSPACE}/${npm_base}"
 
-        echo "Creating file to output to."
+        echo "Creating file for output."
         def timestamp = new Date().format("yyyy-MM-dd_HH-mm-ss", TimeZone.getTimeZone('UTC'))
         def audit_results_file = "npmAudit_${timestamp}.json"
         sh "touch ${audit_results_file}"
 
         echo "Running npm audit"
-        def nodeImage = config.nodeImage ?: "node:10.16.0-stretch-slim" //node:latest
-        docker.image(nodeImage).inside(){
+        def nodeImage = config.node_image ?: "node:latest"
+        docker.image(nodeImage).inside{
             //create package-lock if not already present
             if( !(fileExists("${project_base}/package-lock.json"))) {
                 sh "npm i --prefix ${project_base} --package-lock-only"
