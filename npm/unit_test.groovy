b2c18432505e9759f20b456dd8b63eec85e58f47
Carlos OKieffe <okieffe_carlos@bah.com>
2019-10-02 13:14:50 -0400

    updated unit_test.groovy: changed to config.node_image and added test_install

--- npm/unit_test.groovy
+++ npm/unit_test.groovy
@@ -1,14 +1,21 @@
 void call(){
   stage("NPM Unit test"){
-    def nodeImage = config.nodeImage ?: "node:10.16.0-stretch-slim"
+    def nodeImage = config.node_image ?: "node:latest"
+    def testScript = config.test_cmd ?: "test"
+    def installScript = config.test_install ?: "install"
+
     docker.image(nodeImage).inside{
       def test_results_file = config.test_report_path ?: "junit.xml"
 
       unstash "workspace"
       echo "running npm unit tests"
 
-      String testCmd = config.test_cmd ?: "npm run test"
-      sh "npm install && ${testCmd}"
+      if( installScript ){
+        sh "npm ${installScript}"
+      }
+
+      sh "npm run ${testScript}"
+
       stash name: "workspace",
               includes: config.stash?.includes ?: "**",
               excludes: config.stash?.excludes ?: "node_modules/**",
