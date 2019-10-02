b3116443b6e971773539a3cbf251d9274fd5bbde
Carlos OKieffe <okieffe_carlos@bah.com>
2019-10-02 15:06:48 -0400

    using correct fields

--- npm/library_config.groovy
+++ npm/library_config.groovy
@@ -1,10 +1,12 @@
 fields{
   optional{
-    images{
-      registry = ~/^http(s)?:\/\/[a-zA-Z0-9\-_\.]+(:\d+)?$/
-      cred = ~/^[a-zA-Z0-9\-_\.]+$/
-      repository = ~/^[a-zA-Z0-9\-_\.\/]+$/
-      docker_args = String
-    }
+    node_image = String
+    build_cmd = String
+    build_install = String
+    test_cmd = String
+    test_install = String
+    npm_base = String
+    use_npm_default_registry = Boolean
+    test_report_path = String
   }
 }
