21ac709ca6e4b64562dc57d49d8520538e407321
Carlos OKieffe <okieffe_carlos@bah.com>
2019-10-28 13:39:06 -0400

    feat: adding ecs library; correct usage of app_env.previous and other change based on stevens commentary

--- ecs/deploy_to.groovy
+++ ecs/deploy_to.groovy
@@ -2,16 +2,16 @@ void call(app_env){
    stage("ECS: Deploy To ${app_env.long_name}"){
 
 
-      String region = env.AWS_REGION ?: 
-                     { error "must define AWS region where ecs cluster is location in ecs library." }() 
+      String region = config.aws_region ?: app_env.AWS_REGION ?: env.AWS_REGION ?:
+              { error "must define AWS region where ecs cluster is location in ecs library." }()
 
-      String task = app_env.ECS_TASK_NAME ?:
+      String task = config.ecs_task_name ?: app_env.ECS_TASK_NAME ?:
                   { error "must define AWS ECS Task Name in ecs library." }()
       
-      String cluster = app_env.ECS_CLUSTER_NAME ?:
+      String cluster = config.ecs_cluster_name ?: app_env.ECS_CLUSTER_NAME ?:
                      { error "must define AWS ECS Cluster Name in ecs library." }() 
 
-      String service = app_env.ECS_SERVICE_NAME ?: 
+      String service = config.ecs_service_name ?: app_env.ECS_SERVICE_NAME ?:
                      { error "must define AWS ECS Service Name to update in ecs library." }() 
 
       inside_sdp_image("aws"){
@@ -19,16 +19,11 @@ void call(app_env){
             assumeRole() 
             String image;
             //If deploying to Dev (1st env), expect the image to have been built in the current pipeline run
-            if(app_env.equals(Dev)){
+            if(!app_env.previous){
                def imageInfo = get_images_to_build().first()
                image = "${imageInfo.registry}/${imageInfo.repo}:${imageInfo.tag}"
-               //If deploying to Test, use the image currently in Dev
-            } else if (app_env.equals(Test)){
-               def rawImage = sh(script: "aws ecs describe-task-definition --task-definition ${Dev.ECS_TASK_NAME} --region '${region}' | jq '.taskDefinition | .containerDefinitions[0].image'", returnStdout: true).trim()
-               image = rawImage[1..rawImage.size()-2]
-               //If deploying to Prod, use the image currently in Test
-            } else if (app_env.equals(Prod)){
-               def rawImage = sh(script: "aws ecs describe-task-definition --task-definition ${Test.ECS_TASK_NAME} --region '${region}' | jq '.taskDefinition | .containerDefinitions[0].image'", returnStdout: true).trim()
+            }else{
+               def rawImage = sh(script: "aws ecs describe-task-definition --task-definition ${app_env.previous.ECS_TASK_NAME} --region '${region}' | jq '.taskDefinition | .containerDefinitions[0].image'", returnStdout: true).trim()
                image = rawImage[1..rawImage.size()-2]
             }
             String newRevision = this.createNewTaskRevision(region, task, cluster, service, image)
