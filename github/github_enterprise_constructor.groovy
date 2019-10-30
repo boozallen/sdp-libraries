7b0a426dd9e56d949a2a05dbfb19de3776f9fb20
Carlos OKieffe <okieffe_carlos@bah.com>
2019-10-30 16:07:46 -0400

    fix: adding hooks.Init import to fix unit test failure

--- github/github_enterprise_constructor.groovy
+++ github/github_enterprise_constructor.groovy
@@ -2,6 +2,7 @@
   Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
+import org.boozallen.plugins.jte.hooks.Init
 
 @Init
 void call(Map context) {
