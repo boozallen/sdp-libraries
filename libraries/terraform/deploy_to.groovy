dacebc335dc5f1f4e2daa7c8154f7d364e288757
steven-terrana <steventerrana@gmail.com>
2020-04-18 20:37:05 -0400

    debug

--- libraries/terraform/deploy_to.groovy
+++ libraries/terraform/deploy_to.groovy
@@ -48,7 +48,7 @@ void validateParameters(def app_env){
     LinkedHashMap envSecrets = app_env.terraform?.secrets ?: [:]
     ArrayList errors = []
     (libSecrets + envSecrets).each{ key, secret -> 
-        secret = secret.getValue()
+        secret = secret.values()
         if(!secret.id){
             errors << "secret '${key}' must define 'id'"
         }
