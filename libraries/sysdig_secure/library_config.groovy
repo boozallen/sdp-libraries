d108f66768c7810241b98c7a57c2eb5d91700e4a
steven-terrana <steventerrana@gmail.com>
2020-04-27 11:52:26 -0400

    resolve merge conflicts

--- libraries/sysdig_secure/library_config.groovy
+++ libraries/sysdig_secure/library_config.groovy
@@ -5,9 +5,6 @@ fields{
     optional{
         scan_script_url = String 
         sysdig_secure_url = String
-<<<<<<< HEAD
         enforce_success = Boolean
-=======
->>>>>>> upstream/master
     }
 }
