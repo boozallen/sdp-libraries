c3ef3ebb099eba5e14b3da9945f914295dd1d592
steven-terrana <steventerrana@gmail.com>
2020-04-27 21:20:34 -0400

    debug

--- libraries/sdp/create_workspace_stash.groovy
+++ libraries/sdp/create_workspace_stash.groovy
@@ -2,32 +2,16 @@
   Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
-import org.jenkinsci.plugins.workflow.cps.GlobalVariable
-import org.jenkinsci.plugins.workflow.multibranch.SCMVar
-import hudson.scm.SCM
-import org.boozallen.plugins.jte.console.TemplateLogger
-
 
 @Validate // validate so this runs prior to other @Init steps
 void call(context){
-    println "inside" 
     node{
-        println "fetching scm" 
-        println "scm = ${scm}"
-        if(getSCM()){
-            cleanWs()
-            checkout getSCM()
-        } else {
+        cleanWs()
+        try{
+            checkout scm
+        }catch(any) {
             println "scm var not present, skipping source code checkout" 
         }
         stash name: 'workspace', allowEmpty: true, useDefaultExcludes: false
     }
-}
-
-@NonCPS
-SCM getSCM(){
-    GlobalVariable scmVar = GlobalVariable.byName("scm", currentBuild.rawBuild)
-    SCM scm = scmVar.getValue(this)
-    TemplateLogger.printWarning "scm = ${scm}"
-    return scm
 }
