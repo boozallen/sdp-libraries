b5567c29a1e1cea07112f167b85e74fada26931d
steven-terrana <steventerrana@gmail.com>
2020-04-27 21:28:10 -0400

    debug

--- libraries/sdp/create_workspace_stash.groovy
+++ libraries/sdp/create_workspace_stash.groovy
@@ -3,13 +3,15 @@
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
 
+import hudson.AbortException
+
 @Validate // validate so this runs prior to other @Init steps
 void call(context){
     node{
         cleanWs()
         try{
             checkout scm
-        }catch(any) {
+        }catch(AbortException ex) {
             println "scm var not present, skipping source code checkout" 
         }
         stash name: 'workspace', allowEmpty: true, useDefaultExcludes: false
