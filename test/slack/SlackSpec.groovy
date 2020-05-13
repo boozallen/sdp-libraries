48e3a094513d51cd93af042a697662a1ea893ed9
ottoson-keegan <ottoson_keegan@bah.com>
2020-05-13 11:39:35 -0400

    Slack Lib / Docs - Review/update unit tests; update unit-testing docs

--- test/slack/SlackSpec.groovy
+++ test/slack/SlackSpec.groovy
@@ -3,11 +3,7 @@
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
 
-package slack 
-
-import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
-
-public class SlackSpec extends JenkinsPipelineSpecification {
+public class SlackSpec extends JTEPipelineSpecification {
 
   def SlackTest = null
 
@@ -18,9 +14,9 @@ public class SlackSpec extends JenkinsPipelineSpecification {
 
   def "Successful Build Sends Success Result" () {
     setup:
-      SlackTest.getBinding().setVariable("currentBuild", [ result: "SUCCESS" ])
+      def context = [status: "SUCCESS"]
     when:
-      SlackTest()
+      SlackTest.slack_cleanup(context)
     then:
       1 * getPipelineMock("slackSend")(_ as Map) >> { _arguments ->
         assert _arguments[0]["message"] =~ /Build Successful:.*/
@@ -29,9 +25,9 @@ public class SlackSpec extends JenkinsPipelineSpecification {
 
   def "Failed Build Sends Fail Result" () {
     setup:
-      SlackTest.getBinding().setVariable("currentBuild", [ result: "FAILURE" ])
+      def context = [status: "FAILURE"]
     when:
-      SlackTest()
+      SlackTest.slack_cleanup(context)
     then:
       1 * getPipelineMock("slackSend")(_ as Map) >> { _arguments ->
         assert _arguments[0]["message"] =~ /Build Failure:.*/
@@ -40,10 +36,10 @@ public class SlackSpec extends JenkinsPipelineSpecification {
 
   def "Other Builds Send No Result" () {
     setup:
-      SlackTest.getBinding().setVariable("currentBuild", [ result: "ILLOGICAL" ])
+      def context = [status: "ILLOGICAL"]
       explicitlyMockPipelineVariable("out") //not sure why, but this tests fails w/o this mock
     when:
-      SlackTest()
+      SlackTest.slack_cleanup(context)
     then:
       0 * getPipelineMock("slackSend")(_)
   }
