71895d670c44323633bd69dad69d0c4426b83876
balan-karpagam <balan_karpagam@bah.com>
2020-06-06 13:35:54 -0400

    Trying to catch the exception

--- libraries/sdp/node.groovy
+++ libraries/sdp/node.groovy
@@ -7,14 +7,15 @@
  * An implementtaion of custom node step to be used by SDP library to run a step on a particular node.
  */
 void call(Map nodeConfig = [:],String label = null, Closure body){
-    LinkedHashMap bodyConfig = [:]
     try{
         bodyConfig = body.config
     }catch(MissingPropertyException ex){
         // node invoked from outside a library step
        println (" Call from outside library step ")
-        processNodeCall(label, body, bodyConfig,nodeConfig,"generic")
+       LinkedHashMap bodyConfig = [:]
+       processNodeCall(label, body, bodyConfig,nodeConfig,"generic")
     }
+    LinkedHashMap bodyConfig = [:]
     String agentType = bodyConfig.agentType ?: config.agentType ?: "generic"
     if(!(agentType in ["kubernetes", "docker", "generic"])){
         error "The specified agentType must be one of ['kubernetes', 'docker', 'generic'].  Found '${agentType}'."
