640e7369ceed57faa42171d95e499ae7fe7e1da4
steven-terrana <steventerrana@gmail.com>
2020-07-06 14:57:04 -0400

    patch syntax error

--- libraries/kubernetes/deploy_to.groovy
+++ libraries/kubernetes/deploy_to.groovy
@@ -85,7 +85,7 @@ void call(app_env){
         withKubeConfig([credentialsId: k8s_credential , contextName: k8s_context]) {
             this.update_values_file( values_file, config_repo )
             this.do_release release, values_file
-            this.push_config_update, values_file
+            this.push_config_update values_file
         }
       }
     }
