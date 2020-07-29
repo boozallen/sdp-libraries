4480fc0a45e9e3e2bc28f35a55acbd7c80e286bc
Jesse Alan Johnson <jesseajohnson@pm.me>
2020-07-29 09:57:02 -0400

    Create web_hint_analysis.groovy

---
+++ libraries/webhint/web_hint_analysis.groovy
@@ -0,0 +1,31 @@
+/*
+  Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
+  This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
+*/
+void call(){
+    stage("Webhint: Lint"){
+      
+      /*
+      def url = env.FRONTEND_URL ?: config.url ?: {
+        error """
+        Webhint.io Library needs the target url.
+        libraries{
+          webhint{
+            url = "https://example.com"
+          }
+        }
+        """
+      } ()
+      
+      //println "web hint analysis from the webhint.io library ${url}"
+      
+      node {
+        sh "echo ${url}"
+      }
+      */
+      //inside_sdp_image "webhint", {
+      docker.image("webhint").inside{
+         sh "ls -al; pwd"  
+      }
+    }
+}
