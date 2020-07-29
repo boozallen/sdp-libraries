6dba7bc5de8fc41a84058321d2f7fc3c640aa29c
Jesse Alan Johnson <jesseajohnson@pm.me>
2020-07-29 10:09:45 -0400

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -3,10 +3,10 @@ libraries{
     sonarqube
     webhint
     sdp{
-    images{
-      registry = "https://docker.pkg.github.com"
-      repository = "boozallen/sdp-images"
-      cred = "github"
-    }
+      images{
+        registry = "https://docker.pkg.github.com"
+        repository = "boozallen/sdp-images"
+        cred = "github"
+      }
   }
 }
