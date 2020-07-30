85f32f5f421b0537d81a649151867ce77bfbb1e9
Jesse Alan Johnson <jesseajohnson@pm.me>
2020-07-30 13:40:47 -0400

    Update web_hint_analysis.groovy

--- libraries/webhint/web_hint_analysis.groovy
+++ libraries/webhint/web_hint_analysis.groovy
@@ -17,11 +17,9 @@ void call(){
       } ()
       // write-json
       inside_sdp_image "webhint:latest", {
-        //sh "echo \"{\"extends\":[\"accessibility\"]\",\"formatters\":[\"json\"]} > .hintrc"
-        //sh "cp /hint/.hintrc ."
-        //sh "ls -al /hint"
         sh "cat /hint/.hintrc"
-        //sh "hint ${url}"
+        sh "cp /hint/.hintrc ."
+        sh "hint ${url}"
       }
     }
 }
