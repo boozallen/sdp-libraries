5b0ff930f67ed3bab376fc493902fb2010c37b78
Jesse Alan Johnson <jesseajohnson@pm.me>
2020-07-31 10:46:45 -0400

    Update web_hint_analysis.groovy

--- libraries/webhint/web_hint_analysis.groovy
+++ libraries/webhint/web_hint_analysis.groovy
@@ -4,9 +4,7 @@
 */
 void call(){
     stage("Webhint: Lint") {
-      
-      String resultsDir = "hint-report"
-      
+      def String resultsDir = "hint-report"
       def url = env.FRONTEND_URL ?: config.url ?: {
         error """
         Webhint.io Library needs the target url.
@@ -18,20 +16,18 @@ void call(){
         """
       } ()
       
-      this.createHintrcFile()
-      
       inside_sdp_image "webhint:1.8", {
-        this.makeArchiveStagingDirectory(resultDir)
-        this.createHintrcFile(resultDir)
-
-        String resultsText = "hint.results.txt"
-        String resultsJson = "hint.results.json"
+        def String resultsText = "hint.results.txt"
+        def String resultsJson = "hint.results.json"
+        def hintrc = [
+          extends: [ "accessibility" ],
+          formatters: [ "html", "json" ]
+        ]
         
-        sh script: """
-            cp ${resultsDir}/.hintrc .;
-            cat .hintrc;
-            hint ${url} > ${resultsDir}/${resultsText};
-           """, returnStatus: true
+        sh "mkdir -p ${resultDir}"
+        writeJSON file: "${resultDir}/.hintrc", json: hintrc
+        sh "cp ${resultsDir}/.hintrc .; cat .hintrc;"
+        sh script: "hint ${url} > ${resultsDir}/${resultsText}", returnStatus: true
         
         // hint ${url} always exits non 0 so run cleanup work with separate sh
         // Our goal here to to remove the first two lines which are not valid json
@@ -46,20 +42,6 @@ void call(){
         //this.validateResults("${resultsDir}/${resultsFile}")
       }
     }
-}
-
-void makeArchiveStagingDirectory(String path) {
-     sh "mkdir -p ${path}"
-}
-
-void createHintrcFile(String path) {
-  def hintrc = [
-    extends: [ "accessibility" ],
-    formatters: [ "html", "json" ]
-  ]
-
-  writeJSON file: "${path}/.hintrc", json: hintrc
-  sh "cat ${path}/.hintrc"
 }
 
 void validateResults(String resultsFile) {
