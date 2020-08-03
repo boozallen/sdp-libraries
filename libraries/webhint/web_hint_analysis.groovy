1eba7737e850bc011da7d8be10141148f729eb3b
Jesse Alan Johnson <jesseajohnson@pm.me>
2020-08-03 16:32:39 -0400

    Update web_hint_analysis.groovy

--- libraries/webhint/web_hint_analysis.groovy
+++ libraries/webhint/web_hint_analysis.groovy
@@ -18,7 +18,6 @@ void call(){
       inside_sdp_image "webhint:1.9", {
         String resultsDir = "hint-report"
         String resultsText = "hint.results.log"
-        //String resultsJson = "hint.results.json"
         
         def hintrc = [
           extends: config.extender ?: [ "accessibility" ],
@@ -28,27 +27,11 @@ void call(){
         sh "mkdir -p ${resultsDir}"
         writeJSON file: "${resultsDir}/.hintrc", json: hintrc
         sh "cp ${resultsDir}/.hintrc .; cat .hintrc;"
-        
         sh script: "hint ${url} > ${resultsDir}/${resultsText}", returnStatus: true
-        //sh script: "hint ${url} > ${resultsDir}/${resultsText}", returnStatus: true
-        //sh script: "hint ${url} -f html json -o ${resultsDir}/${resultsJson}", returnStatus: true
         
-        // hint ${url} always exits non 0 so run cleanup work with separate sh
-        // Our goal here to to remove the first two lines which are not valid json
-        // Those lines were included as part of the redirection
-        // This should be a feature request of Webhint.io to create a json file for us like html does
-        /*
-        // Webhint JSON formattor doesn't export a file for us. It sends this content to standard out.
-        // This is hugely promblimatic since the json is intermixed with echos.
-        // Webhint also will output multiple JSON objects depending on its findings
-        // For now, just provide a summary and html. Revisit later when I got time to hand parse the redirection content.
-        sh """
-            tail -n+3 ${resultsDir}/${resultsText} > ${resultsDir}/${resultsJson};
-            if [ ! -s ${resultsDir}/${resultsJson} ] ; then
-              rm ${resultsDir}/${resultsJson}
-            fi
-           """
-        */
+        def lines=new File("${resultsDir}/${resultsText}").readLines()
+        def lastline=lines.get(lines.size()-1)
+        sh "echo ${lastline}"
         archiveArtifacts allowEmptyArchive: true, artifacts: "${resultsDir}/"
         //this.validateResults("${resultsDir}/${resultsJson}")
       }
