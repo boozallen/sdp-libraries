a626c3eea93a73b40d7a67eb2326fe46ba5c91ac
Jesse Alan Johnson <jesseajohnson@pm.me>
2020-08-10 11:40:08 -0400

    Update web_hint_analysis.groovy

--- libraries/webhint/web_hint_analysis.groovy
+++ libraries/webhint/web_hint_analysis.groovy
@@ -3,28 +3,28 @@
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
 void call(){
-    stage("Webhint: Lint") {
-      def url = config.url ?: {
-        error """
-          Webhint.io Library needs the target url.
-          libraries{
-            webhint{
-              url = "https://example.com"
-            }
+  stage("Webhint: Lint") {
+    def url = config.url ?: {
+      error """
+        Webhint.io Library needs the target url.
+        libraries{
+          webhint{
+            url = "https://example.com"
           }
-        """
-      } ()
-      
-      inside_sdp_image "webhint", {
-        String resultsDir = "hint-report"
-        String resultsText = "hint.results.log"
-        
-        this.createAndAddHintrcFile("${resultsDir}")
-        this.processUrl(url, resultsDir, resultsText)
-        archiveArtifacts allowEmptyArchive: true, artifacts: "${resultsDir}/"
-        this.validateResults("${resultsDir}/${resultsText}")
-      }
+        }
+      """
+    } ()
+
+    inside_sdp_image "webhint", {
+      String resultsDir = "hint-report"
+      String resultsText = "hint.results.log"
+
+      this.createAndAddHintrcFile("${resultsDir}")
+      this.processUrl(url, resultsDir, resultsText)
+      archiveArtifacts allowEmptyArchive: true, artifacts: "${resultsDir}/"
+      this.validateResults("${resultsDir}/${resultsText}")
     }
+  }
 }
 
 void createAndAddHintrcFile(String path) {
@@ -44,27 +44,27 @@ void processUrl(String url, String resultsDir, String resultsText) {
 }
 
 void validateResults(String filePath) {
-    if(!fileExists(filePath)) {
-        return
-    }
+  if(!fileExists(filePath)) {
+    return
+  }
   
-    def file = readFile file: "${filePath}"
-    def lines = file.readLines()
-    def lastline=lines.get(lines.size()-1)
-    def total = 0
+  def file = readFile file: "${filePath}"
+  def lines = file.readLines()
+  def lastline=lines.get(lines.size()-1)
+  def total = 0
   
-    for (String item : lastline.split(' ')) {
-      if (item.isNumber()) total += item.toInteger()
-    }
+  for (String item : lastline.split(' ')) {
+    if (item.isNumber()) total += item.toInteger()
+  }
   
-    int fail = config.failThreshold ?: 25
-    int warn = config.warnThreshold ?: 10
+  int fail = config.failThreshold ?: 25
+  int warn = config.warnThreshold ?: 10
 
-    boolean shouldFail = total >= fail
-    boolean shouldWarn = total >= warn
+  boolean shouldFail = total >= fail
+  boolean shouldWarn = total >= warn
 
-    echo "[total hints:${total}] [fail threshold:${fail}] [warn threshold:${warn}]"
+  echo "[total hints:${total}] [fail threshold:${fail}] [warn threshold:${warn}]"
     
-    if(shouldFail) error("Webhint.io found ${total} suggestion(s) meeting or exceeding the fail threshold of ${fail}.")
-    else if(shouldWarn) unstable("Webhint.io found ${total} suggestion(s). Consider fixing a few of them.")
+  if(shouldFail) error("Webhint.io found ${total} suggestion(s) meeting or exceeding the fail threshold of ${fail}.")
+  else if(shouldWarn) unstable("Webhint.io found ${total} suggestion(s). Consider fixing a few of them.")
 }
