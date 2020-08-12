fd4813b73493763c8215b60973652902ddd3d6e8
steven-terrana <steventerrana@gmail.com>
2020-08-11 21:10:31 -0400

    refactor docker build arguments

--- libraries/docker/build.groovy
+++ libraries/docker/build.groovy
@@ -16,14 +16,91 @@ def call(){
       unstash "workspace"
 
       login_to_registry()
-      def arg_string = get_build_arg_string()
       def images = get_images_to_build()
-      images.each{ img ->
-        sh "docker build ${img.context} -t ${img.registry}/${img.repo}:${img.tag} ${arg_string}"
-        sh "docker push ${img.registry}/${img.repo}:${img.tag}"
-        if (remove_local_image) sh "docker rmi -f ${img.registry}/${img.repo}:${img.tag} 2> /dev/null"
+      withBuildArgs{ args ->
+        images.each{ img ->
+          sh "docker build ${img.context} -t ${img.registry}/${img.repo}:${img.tag} ${args}"
+          sh "docker push ${img.registry}/${img.repo}:${img.tag}"
+          if (remove_local_image) sh "docker rmi -f ${img.registry}/${img.repo}:${img.tag} 2> /dev/null"
+        }
       }
 
+    }
+  }
+}
+
+String withBuildArgs(Closure body){
+
+  String buildArgs = []
+  def creds = []
+  
+  config.build_args.each{ argument, value ->
+    if(value instanceof Map){
+      switch(value?.type){
+        case "credential": // validate credential exists and is a secrettext cred
+          def allCreds = CredentialsProvider.lookupCredentials(Credentials, Jenkins.get(),null, null)
+          def cred = allCreds.find{ it.id.equals(value.id) } 
+          if(cred == null){
+              error "docker library: build argument '${argument}' specified credential id '${cred_id}' which does not exist."
+          }
+          if(!(cred instanceof StringCredentialsImpl)){
+            error "docker library: build argument '${argument}' credential must be a Secret Text." 
+          }
+          creds << string(credentialsId: value.id, variable: argument)
+          buildArgs << "--build-arg ${argument}=\$${argument}"
+          break; 
+        case null: // no build argument type provided 
+          error "docker library: build argument '${argument}' must specify a type"
+          break;
+        default: // unrecognized argument type 
+          error "docker library: build argument '${argument}' type of '${value.type}' is not recognized"  
+      }
+    } else {
+      buildArgs << "--build-arg ${argument}=${value}"
+    }
+  }
+
+  withCredentials(creds){
+    body(buildArgs)
+  }
+
+}
+
+/*
+ * @Validate steps run prior to template execution 
+ * if the build_args configuration is present:
+ * 1. it needs to be a map 
+ * 2. if there are build args from credentials, make sure those credentials exist 
+*/
+@Validate
+void call(context){
+  if(!config.containsKey("build_args")){
+    return 
+  }
+
+  if(!(config.build_args instanceof Map)){
+    error "docker library 'build_args' is a ${config.build_args.getClass()} when a block was expected"
+  }
+
+  config.build_args.each{ argument, value ->
+    if(value instanceof Map){
+      switch(value?.type){
+        case "credential": // validate credential exists and is a secrettext cred
+          def allCreds = CredentialsProvider.lookupCredentials(Credentials, Jenkins.get(),null, null)
+          def cred = allCreds.find{ it.id.equals(cred_id) } 
+          if(cred == null){
+              error "docker library: build argument '${argument}' specified credential id '${cred_id}' which does not exist."
+          }
+          if(!(cred instanceof StringCredentialsImpl)){
+            error "docker library: build argument '${argument}' credential must be a Secret Text." 
+          }
+          break; 
+        case null: // no build argument type provided 
+          error "docker library: build argument '${argument}' must specify a type"
+          break;
+        default: // unrecognized argument type 
+          error "docker library: build argument '${argument}' type of '${value.type}' is not recognized"  
+      }
     }
   }
 }
