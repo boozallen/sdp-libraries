310f0890ab66fab87b94d413568a681ce86525d2
CSpicer-BAH <spicer_casey@bah.com>
2020-08-25 17:43:15 -0400

    reconfigured gitlab_status call method

--- libraries/git/gitlab_status.groovy
+++ libraries/git/gitlab_status.groovy
@@ -9,25 +9,39 @@
   jobStatus = Status of job being performed. Accepts the following: "pending", "running", "canceled", "failed", "success"
 */
 
-def call(){
-    glConnection = config.glConnection
-    jobName = config.jobName
-    jobStatus = config.jobStatus
-    call(glConnection, jobName, jobStatus)
-}
-
-def call(String jobStatus){
-    glConnection = config.glConnection
-    jobName = config.jobName
-    call(glConnection, jobName, jobStatus)
-}
-
-def call(String jobName, String jobStatus){
-    glConnection = config.glConnection
-    call(glConnection, jobName, jobStatus)
-}
-
 def call(String glConnection, String jobName, String jobStatus){
-    properties([gitLabConnection(glConnection)])
-    updateGitlabCommitStatus name: jobName , state: jobStatus
+
+  if (glConnection == "" && config.glConnection != ""){
+    glConnection = config.glConnection
+  }
+  else if (glConnection instanceof String){
+
+  }
+  else{
+    error "glConnection must be a string and not empty"
+  }
+
+  if (jobName == "" && config.jobName != ""){
+    jobName = config.jobName
+  }
+  else if (jobName instanceof String){
+
+  }
+  else{
+    error "jobName must be a string and not empty"
+  }
+
+  if (jobStatus == "" && config.jobStatus != ""){
+    jobStatus = config.jobStatus
+  }
+  else if (jobStatus instanceof String){
+
+  }
+  else{
+    error "jobStatus must be a string and not empty"
+  }
+
+  properties([gitLabConnection(glConnection)])
+  updateGitlabCommitStatus name: jobName , state: jobStatus
+
 }
