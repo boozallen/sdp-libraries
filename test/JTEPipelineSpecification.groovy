216744196dd184323e0d867f92b09274cf2b819a
Carlos OKieffe <okieffe_carlos@bah.com>
2020-12-04 15:31:31 -0500

    added loadPipelineScriptForStep

--- test/JTEPipelineSpecification.groovy
+++ test/JTEPipelineSpecification.groovy
@@ -43,4 +43,8 @@ public class JTEPipelineSpecification extends JenkinsPipelineSpecification {
 			return new InvalidlyNamedScriptWrapper( script )
 		}
 	}
+
+	Script loadPipelineScriptForStep(String lib, String step) {	
+		loadPipelineScriptForTest("${lib}/steps/${step}.groovy")
+	}
 }
