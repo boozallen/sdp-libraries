21dc7515a8d4bdab0bdfd41aa2037c6b6f2a6335
Carlos OKieffe <o'kieffe_carlos@bah.com>
2020-12-09 16:51:02 -0500

    checking if gitlab changes are causing serial issues

--- libraries/git/steps/gitlab.groovy
+++ libraries/git/steps/gitlab.groovy
@@ -26,9 +26,9 @@ def get_source_branch(){
         } else {
             projectName = "${env.ORG_NAME}/${env.REPO_NAME}"
         }
-        def gl = this.class.classLoader.loadClass( 'org.gitlab4j.api.GitLabApi', true, false )?.newInstance(gitlabUrl, PAT)
-        sourceBranch =  gl.getMergeRequestApi().getMergeRequest(projectName.toString(), env.CHANGE_ID.toInteger()).getSourceBranch()
-        return sourceBranch
+        //def gl = this.class.classLoader.loadClass( 'org.gitlab4j.api.GitLabApi', true, false )?.newInstance(gitlabUrl, PAT)
+        //sourceBranch =  gl.getMergeRequestApi().getMergeRequest(projectName.toString(), env.CHANGE_ID.toInteger()).getSourceBranch()
+        return null //sourceBranch
     }
   }
 }
