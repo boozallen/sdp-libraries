567adc0378aa0fe5955d0cc5b2613d52317d8c30
Carlos OKieffe <okieffe_carlos@bah.com>
2020-12-15 12:46:28 -0500

    added package to the git library steps

--- libraries/git/steps/gitlab_status.groovy
+++ libraries/git/steps/gitlab_status.groovy
@@ -2,15 +2,13 @@
   Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
+package libraries.git
 
 /*
   glConnection = Name assigned to the GitLab connection set when configuring Jenkins.
   jobName = Name of the pipeline job triggered in Gitlab. Typically "<jenkins-service-account>/branch"
   jobStatus = Status of job being performed. Accepts the following: "pending", "running", "canceled", "failed", "success"
 */
-
-
-
 def call(String gl_connection = null, String gl_job_name = null, String gl_job_status = null){
 
   def gitlab = config.gitlab ?: [:]
