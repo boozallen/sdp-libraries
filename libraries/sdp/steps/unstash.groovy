690bbd3c5fabcef402e354b035fdb65992842bee
steven-terrana <steventerrana@gmail.com>
2021-03-25 18:55:13 -0400

    Update unstash.groovy

--- libraries/sdp/steps/unstash.groovy
+++ libraries/sdp/steps/unstash.groovy
@@ -2,9 +2,7 @@ import org.boozallen.plugins.jte.util.TemplateLogger
 
 void call(String stashName){
   try{
-    if( new File(".git").exists() ){
-      sh "rm -rf .git"
-    }
+    sh " [ -d '.git' ] && rm -rf .git "
   }catch(any){
     TemplateLogger.createDuringRun().printWarning(any.getMessage())
   }
