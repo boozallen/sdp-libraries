8c879ef49b667c5161da05f962b2d37a6fe2c75b
JP <21163267+jpke@users.noreply.github.com>
2021-05-28 15:13:59 -0400

    adding npm lib (#137)

---
+++ libraries/npm/steps/unit_test.groovy
@@ -0,0 +1,10 @@
+/*
+  Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
+  This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
+*/
+
+package libraries.npm.steps
+
+void call(app_env = []){
+  npm_invoke("unit_test", app_env)
+}
