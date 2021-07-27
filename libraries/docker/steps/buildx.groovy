b1d4ff54d9db49249aa7c27d6ead103eed502288
Holt_Joshua <Holt_Joshua@bah.com>
2021-07-27 13:14:16 -0400

    added buildx changes

---
+++ libraries/docker/steps/buildx.groovy
@@ -0,0 +1,125 @@
+package libraries.docker.steps
+
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl
+import com.cloudbees.plugins.credentials.Credentials
+
+void call(){
+  stage "Building Multiarch Docker Image", {
+
+    // Build Strategy must be set to buildx in order for this method to work
+    if (!(config.build_strategy == "buildx")) {
+      error "build strategy: ${config.build_strategy} is not buildx"
+    }
+
+    // This is so that if jenkins agents are running with a version of docker that only had buildx in the experimental features 
+    // then it will set the experimental flag
+    boolean setExperimentalFlag = config.containsKey("setExperimentalFlag") ? config.setExperimentalFlag : false 
+
+    node{
+      unstash "workspace"
+      
+      login_to_registry{
+        // returns array of images to build
+        ArrayList images = get_images_to_build()
+
+        String experimentalFlag = ""
+        if (setExperimentalFlag) {
+          experimentalFlag = "DOCKER_CLI_EXPERIMENTAL=enabled"
+        }
+
+        // create the builder and tell it to spin it up in a container
+        sh "${experimentalFlag} docker buildx create --name smartbuilder --driver docker-container --use"
+
+        images.each{ img -> 
+            echo "building image"
+            // returns the platforms to build for this image in the format needed for buildx
+            String platforms = getPlatforms(img.platforms)
+            // each image in the array has its own build arguments so get this specific image's arguments
+            def returnedArgs = getBuildArgs(img.build_args)
+            def creds = returnedArgs["creds"]
+            def args = returnedArgs["args"]
+
+            String latestTag = ""
+            // if the latestTag is used then add the additional tag flag
+            if(img.useLatestTag ){
+              latestTag = "-t ${img.registry}/${img.repo}:latest"
+            }
+
+            try {
+              withCredentials(creds) {
+                  //this experimental=enabled variable allows for buildx to be used on the current docker ce version. later versions do not need this flag
+                  //when the docker version gets updates in the jenkins agent then this can be changed. 
+                  sh "${experimentalFlag} docker buildx build  ${img.context} -t ${img.registry}/${img.repo}:${img.tag} ${latestTag} ${args} ${platforms} --push"
+              }
+            } catch (any) {
+                  error "error building and pushing multiarchitecture image"
+                  sh "${experimentalFlag} docker buildx rm smartbuilder"
+            }             
+        }
+        //remove the builder so that it is not left hanging on the node
+        sh "${experimentalFlag} docker buildx rm smartbuilder"
+      }
+    }
+  }
+}
+
+def getBuildArgs(def build_args){
+
+  echo "Getting Build Args"
+
+  ArrayList buildArgs = []
+  def creds = []
+
+  build_args.each{ argument, value ->
+    if(value instanceof Map){
+      switch(value?.type){
+        case "credential": // validate credential exists and is a secrettext cred
+          def allCreds = CredentialsProvider.lookupCredentials(Credentials, Jenkins.get(),null, null)
+          def cred = allCreds.find{ it.id.equals(value.id) }
+          if(cred == null){
+              error "docker library: build argument '${argument}' specified credential id '${value.id}' which does not exist."
+          }
+          if(!(cred instanceof StringCredentialsImpl)){
+            error "docker library: build argument '${argument}' credential must be a Secret Text."
+          }
+          creds << string(credentialsId: value.id, variable: argument)
+          buildArgs << "--build-arg ${argument}=\$${argument}"
+          break;
+        case null: // no build argument type provided
+          error "docker library: build argument '${argument}' must specify a type"
+          break;
+        default: // unrecognized argument type
+          error "docker library: build argument '${argument}' type of '${value.type}' is not recognized"
+      }
+    } else {
+      buildArgs << "--build-arg ${argument}='${value}'"
+    }
+  }
+
+  //due to each image having its own build args and credentials, returning this map is necessary so that each image can set the creds properly
+  def returnList = ["args": buildArgs.join(" "), "creds": creds]
+
+  return returnList
+}
+
+String getPlatforms(def platformsArr) {
+  echo "Getting platforms to build image for"
+
+  String platforms = "--platform"
+
+  // default to amd64 if platforms are not set
+  if(platformsArr.size() == 0){
+    return platforms + " linux/amd64"
+  }
+
+  platformsArr.eachWithIndex { platform, index -> 
+
+    if (index == 0){
+      platforms += " ${platform}"
+    } else {
+          platforms += ",${platform}"
+    }
+  }
+  return platforms
+}
--- libraries/docker/steps/get_images_to_build.groovy
+++ libraries/docker/steps/get_images_to_build.groovy
@@ -21,7 +21,7 @@ def call(){
     def (image_reg) = get_registry_info() // config.registry
     def path_prefix = config.repo_path_prefix ? config.repo_path_prefix + "/" : ""
 
-    def build_strategies = [ "docker-compose", "modules", "dockerfile" ]
+    def build_strategies = [ "docker-compose", "modules", "dockerfile", "buildx" ]
     if (config.build_strategy)
     if (!(config.build_strategy in build_strategies))
       error "build strategy: ${config.build_strategy} not one of ${build_strategies}"
@@ -42,6 +42,9 @@ def call(){
           ])
         }
         break
+      case "buildx":
+        images = buildx(image_reg,path_prefix)
+        break
       case "dockerfile": //same as null/default case
       case null:
         images.push([
@@ -52,6 +55,82 @@ def call(){
         ])
         break
     }
-
+    
     return images
+}
+
+ArrayList buildx(image_reg, path_prefix) {
+
+  ArrayList images = []
+
+  def buildx = config.buildx ?: { error "buildx not defined in Pipeline Config" } ()
+  def same_repo_different_tags = config.same_repo_different_tags ?: false
+
+  buildx.each{ name,img -> 
+
+    String repo = ""
+    String context = ""
+    String tag = ""
+    boolean useLatestTag = false
+    boolean latestTagUsed = false
+
+    // if the pipeline config does not contain a context to where the dockerfile is then look at root
+    if (!img.containsKey("context")) {
+      context = "."
+    } else {
+      context = img.context
+    }
+    // if a custom tag is to be used then it can be specified. otherwise it will use the git sha
+    if (!img.containsKey("tag")) {
+      tag += env.GIT_SHA
+    } else {
+      tag += img.tag
+    }
+    // if you need a latest tag in addition to the defined tag or the git sha tag
+    if(img.containsKey("useLatestTag")) {
+        // if the pipeline has useLatestTag set to true and is trying to build for the same repo
+        if (img.useLatestTag && same_repo_different_tags) {
+          // check if latest flag has already been used for the repo and if it hasnt then set the latest tag
+          if (!latestTagUsed) {
+            useLatestTag = img.useLatestTag
+            latestTagUsed = true
+          }
+          //else error because it should not override the other image that previously set the latest flag
+          else {
+            error "Cannot use multiple latest tags if you are building multiple images for the same repo"
+          }
+          // if not building with the same repo name or useLatestTag is set to false then use the config value
+        } else {
+          useLatestTag = img.useLatestTag
+        }
+    }
+
+    //if sameNameDifferentTags flag is set then it will keep the repo name the same and appends the element name to the tag
+    if (same_repo_different_tags) {
+      tag += "-" + name
+      repo = "${path_prefix}${env.REPO_NAME}".toLowerCase()
+    }
+    else {
+      // if there is only one element in buildx array than it does not need the name variable from the pipeline config to make the repo unique so it will skip this step
+      // othwise if it is building more than one image, than each image should have a unique name so it appends the element name to the repo
+      if (!(buildx.size() == 1)) {
+          repo = "${path_prefix}${env.REPO_NAME}_${name}".toLowerCase()
+        }else {
+          repo = "${path_prefix}${env.REPO_NAME}".toLowerCase()
+        }      
+    }
+
+    //push the image into the array for the buildx method to use
+    images.push([
+      registry: image_reg,
+      repo: repo,
+      tag: tag,
+      context: context,
+      build_args: img.build_args,
+      platforms: img.platforms,
+      useLatestTag: useLatestTag
+    ])
+  }
+
+  return images
 }
