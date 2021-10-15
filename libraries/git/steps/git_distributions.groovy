f7ee0e7f40fc734027d143d16bed3549d635e0a8
Connor Younglund <younglund_connor@bah.com>
2021-10-15 16:27:13 -0400

    re-add getStepFromCollector with @NonCPS annotation

--- libraries/git/steps/git_distributions.groovy
+++ libraries/git/steps/git_distributions.groovy
@@ -73,19 +73,24 @@ def fetch(){
   if(jteVersion.lessThanOrEqualTo("2.0.4")){
     return getBinding().getStep(stepName)
   } else {
-    try {
-      Class collector = Class.forName("org.boozallen.plugins.jte.init.primitives.TemplatePrimitiveCollector")
-      List steps = collector.current().getSteps(stepName)
-      
-      if(steps.size() == 0){
-        error "Step '${stepName}' not found."
-      } else if (steps.size() > 1){
-        error "Ambiguous step name '${stepName}'. Found multiple steps by that name."
-      } else {
-        return steps.first().getValue(null)
-      }
-    } catch(ClassNotFoundException ex) {
-      error "can't find the TemplatePrimitiveCollector class. That's odd. current JTE version is '${jteVersion.get()}'. You should submit an issue at https://github.com/jenkinsci/templating-engine-plugin/issues/new/choose"
+    return this.getStepFromCollector(stepName)
+  }
+}
+
+@NonCPS
+def getStepFromCollector(String stepName){
+  try {
+    Class collector = Class.forName("org.boozallen.plugins.jte.init.primitives.TemplatePrimitiveCollector")
+    List steps = collector.current().getSteps(stepName)
+
+    if(steps.size() == 0){
+      error "Step '${stepName}' not found."
+    } else if (steps.size() > 1){
+      error "Ambiguous step name '${stepName}'. Found multiple steps by that name."
+    } else {
+      return steps.first().getValue(null)
     }
+  } catch(ClassNotFoundException ex) {
+    error "can't find the TemplatePrimitiveCollector class. That's odd. current JTE version is '${jteVersion.get()}'. You should submit an issue at https://github.com/jenkinsci/templating-engine-plugin/issues/new/choose"
   }
 }
