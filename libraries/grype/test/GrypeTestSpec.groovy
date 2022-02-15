9b090424dad37815ff24a03b430771d8e4fa159c
mackeyaj <mackey_adam@bah.com>
2022-02-15 16:21:14 -0500

    push GrtypeTestSpec

--- libraries/grype/test/GrypeTestSpec.groovy
+++ libraries/grype/test/GrypeTestSpec.groovy
@@ -14,16 +14,14 @@ class GrypeTestSpec extends JTEPipelineSpecification {
 def setup() {
   GrypeTest = loadPipelineScriptForTest("grype/steps/grype_scan.groovy")
   explicitlyMockPipelineVariable("get_images_to_build")
-  explicitlyMockPipelineVariable("registry-creds")
 
   getPipelineMock("get_images_to_build.call")() >> {
       def images = []
       images << [registry: "reg1", repo: "repo1", context: "context1", tag: "tag1"]
       images << [registry: "reg2", repo: "repo2", context: "context2", tag: "tag2"]
       return images
-  }
-
-  
+    }
+  GrypeTest.getBinding().setVariable("registry-creds", ["user", "pass"])
 }
 
 def "If grype.yaml does not exist, throw error" () {
@@ -35,15 +33,14 @@ def "If grype.yaml does not exist, throw error" () {
     1 * getPipelineMock("error")("no grype config found") >> {throw new DummyException("config file error")}
 }
 
-def "Images are saved from get_images_to_build with docker" () {
+def "Images are saved from get_images_to_build with docker" () {  
+  setup: 
+    
   when:
     GrypeTest()
   then:
-    1 * getPipelineMock("sh")("docker save reg1/repo1:tag1 > tag1.tar ")
-    1 * getPipelineMock("docker.withRegistry")("https://registry.uip.sh/", "registry-creds")
-    1 * getPipelineMock("docker.image")('"registry.uip.sh/toolkit/grype:latest").inside()')
-
-
+  1 * getPipelineMock("sh")("docker save reg1/repo1:tag1 > tag1.tar ")  
+//getPipelineMock("sh")("grype ${img.tag}.tar -o json >> ${RAW_RESULTS_FILE}")
 }
 
 
