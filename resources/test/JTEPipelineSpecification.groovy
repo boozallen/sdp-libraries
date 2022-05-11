bd2e9cfc396d0e84e9b0b520aee5247ba95df916
steven-terrana <steventerrana@gmail.com>
2022-05-11 10:32:38 -0400

    Update JTEPipelineSpecification.groovy

--- resources/test/JTEPipelineSpecification.groovy
+++ resources/test/JTEPipelineSpecification.groovy
@@ -13,17 +13,17 @@ public class JTEPipelineSpecification extends JenkinsPipelineSpecification {
     @Override
     Script loadPipelineScriptForTest(String _path) {
 		
-		String[] path_parts = _path.split( "/" )
-		
-		String filename = path_parts[path_parts.length-1]
-		
-		String resource_path = "/"
-		if( path_parts.length >= 2 ) {
-			resource_path = String.join( "/", path_parts[0..path_parts.length-2] )
-			resource_path = "/${resource_path}/"
-		}
+	String[] path_parts = _path.split( "/" )
 
-		GroovyScriptEngine script_engine = new GroovyScriptEngine(generateScriptClasspath(resource_path))
+	String filename = path_parts[path_parts.length-1]
+
+	String resource_path = "/"
+	if( path_parts.length >= 2 ) {
+		resource_path = String.join( "/", path_parts[0..path_parts.length-2] )
+		resource_path = "/${resource_path}/"
+	}
+
+	GroovyScriptEngine script_engine = new GroovyScriptEngine(generateScriptClasspath(resource_path))
         CompilerConfiguration cc = script_engine.getConfig() 
         // define auto importing of JTE hook annotations
         ImportCustomizer ic = new ImportCustomizer()
@@ -33,16 +33,10 @@ public class JTEPipelineSpecification extends JenkinsPipelineSpecification {
         script_engine.setConfig(cc)
 
 		Class<Script> script_class = script_engine.loadScriptByName( "${filename}" )
-
 		Script script = script_class.newInstance()
-
-		addPipelineMocksToObjects( script )
+		addPipelineMocksToObjects(script)
 		
-		if( SourceVersion.isName( script_class.getSimpleName() ) ) {
-			return script
-		} else {
-			return new InvalidlyNamedScriptWrapper( script )
-		}
+		return SourceVersion.isName(script_class.getSimpleName()) ? script : new InvalidlyNamedScriptWrapper(script)
 	}
 
 	Script loadPipelineScriptForStep(String lib, String step) {	
