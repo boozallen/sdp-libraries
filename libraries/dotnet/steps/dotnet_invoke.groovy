adf837886a434563b01d34aa91bc7ab0e8c0a4af
mackeyaj <mackey_adam@bah.com>
2022-06-01 12:22:52 -0400

    formatting fixes

--- libraries/dotnet/steps/dotnet_invoke.groovy
+++ libraries/dotnet/steps/dotnet_invoke.groovy
@@ -3,6 +3,7 @@
   This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 */
 
+package libraries.dotnet.steps
 
 @StepAlias(["source_build", "unit_test"])
 void call() {
@@ -20,109 +21,39 @@ void call() {
             resultDir = config.unit_test.resultDir ?: "coverage"
             break
         default:
-                error("stepName must be \"source_build\" or \"unit_test\" got \"${stepName}\"") //doesnt look right
+            error("step name must be \"source_build\" or \"unit_test\" got \"${stepContext.name}\"")
     }
 
     stage(stepName) {
+        inside_sdp_image "dotnet-sdk:5.0.214", {
+            unstash "workspace"
 
-        // run dotnet command in dotnet container
-		docker.withRegistry("https://registry.uip.sh/", "registry-creds") {
-            // need to publish dotnet image to sdp-images prior to swicting to inside sdp image
-      	    docker.image("registry.uip.sh/toolkit/dotnet-sdk-builder:latest").inside{
-
-                unstash "workspace"
-
-                if(stepName == "DotNet Build") {
-                                   
-                    try{
+            if (stepName == "DotNet Build") {
+                try {
                     sh "dotnet publish -c release -o ${outDir}"
-                    }
-                    catch (any){
-                        throw any
-                    }
-                    finally{
-                    archiveArtifacts artifacts: "${outDir}/*.*, allowEmptyArchive: true"
-                    }
                 }
-                
-                else {
-                    
-
-                    //Execute dotnet tests and output to coverage directory
-                    try{
-                    sh "rm -drf ${resultDir}"
-                    sh "dotnet test --collect:'XPlat Code Coverage' --results-directory  --logger trx" //${resultDir}
-                    }
-
-                    catch (any){
-                        throw(any)
-                    }
-                    finally{
-                    archiveArtifacts artifacts: "${resultDir}/**/*, allowEmptyArchive: true"
-                    }
-
-                stash "workspace"
+                catch (any) {
+                    throw any
+                }
+                finally {
+                    archiveArtifacts artifacts: "${outDir}/*.*, allowEmptyArchive: true"
                 }
             }
+            else {
+                //Execute dotnet tests and output to coverage directory
+                try {
+                    sh "rm -drf ${resultDir}"
+                    sh "dotnet test --collect:'XPlat Code Coverage' --results-directory  --logger trx" //${resultDir}
+                }
+                catch (any) {
+                    throw any
+                }
+                finally {
+                    archiveArtifacts artifacts: "${resultDir}/**/*, allowEmptyArchive: true"
+                }
+            }
+
+            stash "workspace"
         }
-    }
-}                       
-        
-
-void validateSecrets(secrets) {
-    ArrayList errors = []
-    secrets.keySet().each{ key ->
-        def secret = secrets[key]
-        println "secret -> ${secret}"
-        if (!secret.id) {
-            errors << "secret '${key}' must define 'id'"
-        }
-        switch(secret.type) {
-            case "text":
-                if (!secret.name) errors << "secret '${key}' must define 'name'" 
-                break
-            case "usernamePassword":
-                if (!secret.usernameVar) errors << "secret '${key}' must define 'usernameVar'"
-                if (!secret.passwordVar) errors << "secret '${key}' must define 'passwordVar'"
-                break
-            default:
-                errors << "secret '${key}': type '${secret.type}' is not defined"
-        }
-    }
-
-    if (errors) {
-        error (["DotNet Library Validation Errors: "] + errors.collect{ "- ${it}"})?.join("\n")
-    }
-}
-
-ArrayList formatSecrets(libStepConfig, appStepConfig) {
-    LinkedHashMap libSecrets = libStepConfig?.env?.secrets ?: [:]
-    LinkedHashMap envSecrets = appStepConfig?.env?.secrets ?: [:]
-    LinkedHashMap secrets = libSecrets + envSecrets
-
-    this.validateSecrets(secrets)
-
-    ArrayList creds = [] 
-    secrets.keySet().each{ key -> 
-        def secret = secrets[key]
-        switch(secret.type) {
-            case "text": 
-                creds << string(credentialsId: secret.id, variable: secret.name)
-                break
-            case "usernamePassword":
-                creds << usernamePassword(credentialsId: secret.id, usernameVariable: secret.usernameVar, passwordVariable: secret.passwordVar)
-                break
-        }
-    }
-    return creds
-}
-
-void setEnvVars(libStepConfig, appStepConfig) {
-    LinkedHashMap libEnv = libStepConfig?.env?.findAll { it.key != 'secrets' } ?: [:]
-    LinkedHashMap appEnv = appStepConfig?.env?.findAll { it.key != 'secrets' } ?: [:]
-    LinkedHashMap envVars = libEnv + appEnv
-
-    envVars.each {
-        env[it.key] = it.value
     }
 }
