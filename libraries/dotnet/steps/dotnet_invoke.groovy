24c4e5e8ab927b235eddc0150736ba73639c34e6
mackeyaj <97459289+mackeyaj@users.noreply.github.com>
2022-06-02 10:43:23 -0400

    Adds .Net Library (#159)
    * add dotnet library
    
    * dotnet t/s
    
    * changed class name back
    
    * unit test test
    
    * testing
    
    * unit test dev
    
    * formatting fix
    
    * formatting fixes
    
    * working unit test
    
    * test mocking sh commands
    
    * add jenv file to gitignore
    
    * Delete .DS_Store
    
    * Delete .java-version
    
    Co-authored-by: Connor <younglund_connor@bah.com>

---
+++ libraries/dotnet/steps/dotnet_invoke.groovy
@@ -0,0 +1,59 @@
+/*
+  Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
+  This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
+*/
+
+package libraries.dotnet.steps
+
+@StepAlias(["source_build", "unit_test"])
+void call() {
+    String stepName = ""
+    String outDir = ""
+    String resultDir = ""
+
+    switch(stepContext.name) {
+        case "source_build":
+            stepName = "DotNet Build"
+            outDir = config.source_build.outDir ?: "bin"
+            break
+        case "unit_test":
+            stepName = "DotNet Unit Test"
+            resultDir = config.unit_test.resultDir ?: "coverage"
+            break
+        default:
+            error("step name must be \"source_build\" or \"unit_test\" got \"${stepContext.name}\"")
+    }
+
+    stage(stepName) {
+        inside_sdp_image "dotnet-sdk:5.0.214", {
+            unstash "workspace"
+
+            if (stepName == "DotNet Build") {
+                try {
+                    sh "dotnet publish -c release -o ${outDir}"
+                }
+                catch (any) {
+                    throw any
+                }
+                finally {
+                    archiveArtifacts artifacts: "${outDir}/*.*, allowEmptyArchive: true"
+                }
+            }
+            else {
+                //Execute dotnet tests and output to coverage directory
+                try {
+                    sh "rm -drf ${resultDir}"
+                    sh "dotnet test --collect:'XPlat Code Coverage' --results-directory ${resultDir} --logger trx"
+                }
+                catch (any) {
+                    throw any
+                }
+                finally {
+                    archiveArtifacts artifacts: "${resultDir}/**/*, allowEmptyArchive: true"
+                }
+            }
+
+            stash "workspace"
+        }
+    }
+}
