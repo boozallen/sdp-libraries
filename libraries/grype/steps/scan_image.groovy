a7c92db37a03d4f1bd8f9c2d96022c7dbfbded65
mackeyaj <mackey_adam@bah.com>
2022-06-09 10:43:02 -0400

    add scan_image

---
+++ libraries/grype/steps/scan_image.groovy
@@ -0,0 +1,46 @@
+void call() {
+  stage("Grype Image Scan") {
+    def GRYPE_CONFIG = ".grype.yaml"
+    def RAW_RESULTS_FILE = "grype-scan-results.json"
+    def TRANSFORMED_RESULSTS_FILE = "grype-scan-results.txt"
+
+    if (!fileExists("./${GRYPE_CONFIG}")) { error "no grype config found" }
+
+    def images = get_images_to_build()
+        
+    images.each { img ->
+      docker.withRegistry("https://registry.uip.sh/", "registry-creds") {
+        docker.image("registry.uip.sh/toolkit/grype:0.38.0").inside() {
+          
+          // perform the grype scan
+          try {
+            sh "grype ${img.registry}/${img.repo}:${img.tag} -o json >> ${RAW_RESULTS_FILE}"
+          
+            echo "No CVE's at or above set threshold!"
+          }
+          
+          // Catch the error on quality gate failure
+          catch(Exception err) {
+            echo "Failed: ${err}"
+          
+            echo "Grype Quality Gate Failed. There are one or more CVE's that exceed the maximum allowed severity rating!"
+          
+            throw err
+          }
+
+          // display the results in a human-readable format
+          finally {
+            def transform_script = resource("transform-grype-scan-results.sh")
+            writeFile file: "transform-results.sh", text: transform_script
+          
+            def transformed_results = sh script: "/bin/bash ./transform-results.sh ${RAW_RESULTS_FILE} ${GRYPE_CONFIG}", returnStdout: true
+            writeFile file: TRANSFORMED_RESULSTS_FILE, text: transformed_results.trim()
+
+           // archive the results
+            archiveArtifacts artifacts: "${RAW_RESULTS_FILE}, ${TRANSFORMED_RESULSTS_FILE}"
+          }
+        }
+      }
+    }
+  }
+}
