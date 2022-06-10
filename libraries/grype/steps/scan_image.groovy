dded198dcea57101dd1691df1b8a878516a2f70a
mackeyaj <mackey_adam@bah.com>
2022-06-10 16:22:31 -0400

    test if statement

--- libraries/grype/steps/scan_image.groovy
+++ libraries/grype/steps/scan_image.groovy
@@ -21,10 +21,11 @@ void call() {
             if (severityThreshold == "none") {
               sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} >> ${rawResultsFile}"
             }
-
+            else {
             sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} --fail-on ${severityThreshold} >> ${rawResultsFile}"
-          
+            
             echo "No CVE's at or above set threshold!"
+            }
           }
           
           // Catch the error on quality gate failure
