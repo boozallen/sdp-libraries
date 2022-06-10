12636ee686f2de3431bf84a64776db63af4422e8
mackeyaj <mackey_adam@bah.com>
2022-06-10 14:38:26 -0400

    change upCase vars to camelcase, added config vars

--- libraries/grype/steps/scan_image.groovy
+++ libraries/grype/steps/scan_image.groovy
@@ -1,8 +1,10 @@
 void call() {
   stage("Grype Image Scan") {
-    def String GRYPE_CONFIG = ".grype.yaml"
-    def String RAW_RESULTS_FILE = "grype-scan-results.json"
-    def String TRANSFORMED_RESULSTS_FILE = "grype-scan-results.txt"
+    def String grypeConfig = ".grype.yaml"
+    def String rawResultsFile = "grype-scan-results.json"
+    def String transformedResultsFile = "grype-scan-results.txt"
+    def String outputFormat = config?.reportFormat ?: "json"
+    def String severityThreshold = config?.failOnSeverity ?: "high"
     def images = get_images_to_build()
         
     images.each { img ->
@@ -10,11 +12,11 @@ void call() {
         docker.image("registry.uip.sh/toolkit/grype:0.38.0").inside() {
           unstash "workspace"
           //check for grype config file in workspace
-          if (!fileExists("./${GRYPE_CONFIG}")) { error "no grype config found" }
+          if (!fileExists("./${grypeConfig}")) { error "no grype config found" }
 
           // perform the grype scan
           try {
-            sh "grype ${img.registry}/${img.repo}:${img.tag} -o json >> ${RAW_RESULTS_FILE}"
+            sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} --fail-on ${severityThreshold} >> ${rawResultsFile}"
           
             echo "No CVE's at or above set threshold!"
           }
@@ -33,11 +35,11 @@ void call() {
             def transform_script = resource("transform-grype-scan-results.sh")
             writeFile file: "transform-results.sh", text: transform_script
           
-            def transformed_results = sh script: "/bin/bash ./transform-results.sh ${RAW_RESULTS_FILE} ${GRYPE_CONFIG}", returnStdout: true
-            writeFile file: TRANSFORMED_RESULSTS_FILE, text: transformed_results.trim()
+            def transformed_results = sh script: "/bin/bash ./transform-results.sh ${rawResultsFile} ${grypeConfig}", returnStdout: true
+            writeFile file: transformedResultsFile, text: transformed_results.trim()
 
            // archive the results
-            archiveArtifacts artifacts: "${RAW_RESULTS_FILE}, ${TRANSFORMED_RESULSTS_FILE}"
+            archiveArtifacts artifacts: "${rawResultsFile}, ${transformedResultsFile}"
             stash "workspace"
           }
         }
