843bac14f193646beb60cd6ac08b024aa3134d5d
mackeyaj <mackey_adam@bah.com>
2022-06-13 16:53:01 -0400

    formatting

--- libraries/grype/steps/scan_image.groovy
+++ libraries/grype/steps/scan_image.groovy
@@ -1,64 +1,58 @@
 package libraries.grype.steps
 
 void call() {
-  stage("Grype Image Scan") {
-      String grypeConfig = ".grype.yaml"
-      String outputFormat = config?.report_format ?: "json"
-      String severityThreshold = config?.fail_on_severity ?: "high"
-    
-        
-    
-      docker.withRegistry("https://registry.uip.sh/", "registry-creds") {
-        docker.image("registry.uip.sh/toolkit/grype:0.38.0").inside() {
-          unstash "workspace"
-          def images = get_images_to_build()
-          images.each { img ->
-          String rawResultsFile = "${img.tag}-grype-scan-results.json"
-          //check for grype config file in workspace
-          if (!fileExists("./${grypeConfig}")) { error "no grype config found" }
-
-          // perform the grype scan
-          try {
-            if (severityThreshold == "none") {
-              sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} >> ${rawResultsFile}"
-            }
-            else {
-            sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} --fail-on ${severityThreshold} >> ${rawResultsFile}"
+    stage("Grype Image Scan") {
+        String grypeConfig = ".grype.yaml"
+        String outputFormat = config?.report_format ?: "json"
+        String severityThreshold = config?.fail_on_severity ?: "high"
             
-            echo "No CVE's at or above set threshold!"
+        docker.withRegistry("https://registry.uip.sh/", "registry-creds") {
+            docker.image("registry.uip.sh/toolkit/grype:0.38.0").inside() {
+                unstash "workspace"
+                def images = get_images_to_build()
+                images.each { img ->
+                    String rawResultsFile = "${img.tag}-grype-scan-results.json"
+                    //check for grype config file in workspace
+                    if (!fileExists("./${grypeConfig}")) { error "no grype config found" }
+    
+                    // perform the grype scan
+                    try {
+                        if (severityThreshold == "none") {
+                          sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} >> ${rawResultsFile}"
+                        }
+                        else {
+                        sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} --fail-on ${severityThreshold} >> ${rawResultsFile}"
+                        
+                        echo "No CVE's at or above set threshold!"
+                        }
+                    }
+              
+                    // Catch the error on quality gate failure
+                    catch(Exception err) {
+                        echo "Failed: ${err}"
+                        echo "Grype Quality Gate Failed. There are one or more CVE's that exceed the maximum allowed severity rating!"
+                        throw err
+                    }
+                    // display the results in a human-readable format
+                    finally {
+                        if (outputFormat == "json") {
+                            String transformedResultsFile = "${img.tag}-grype-scan-results.txt"
+                            def transform_script = resource("transform-grype-scan-results.sh")
+                            writeFile file: "transform-results.sh", text: transform_script
+                          
+                            def transformed_results = sh script: "/bin/bash ./transform-results.sh ${rawResultsFile} ${grypeConfig}", returnStdout: true
+                            writeFile file: transformedResultsFile, text: transformed_results.trim()
+              
+                            // archive the results
+                            archiveArtifacts artifacts: "${rawResultsFile}, ${transformedResultsFile}"
+                        }
+                        else {
+                            archiveArtifacts artifacts: "${rawResultsFile}"
+                        }
+                      stash "workspace"
+                    }
+                }
             }
-          }
-          
-          // Catch the error on quality gate failure
-          catch(Exception err) {
-            echo "Failed: ${err}"
-          
-            echo "Grype Quality Gate Failed. There are one or more CVE's that exceed the maximum allowed severity rating!"
-          
-            throw err
-          }
-
-          // display the results in a human-readable format
-          finally {
-            if (outputFormat == "json") {
-              String transformedResultsFile = "${img.tag}-grype-scan-results.txt"
-              def transform_script = resource("transform-grype-scan-results.sh")
-              writeFile file: "transform-results.sh", text: transform_script
-            
-              def transformed_results = sh script: "/bin/bash ./transform-results.sh ${rawResultsFile} ${grypeConfig}", returnStdout: true
-              writeFile file: transformedResultsFile, text: transformed_results.trim()
-
-              // archive the results
-              archiveArtifacts artifacts: "${rawResultsFile}, ${transformedResultsFile}"
-            }
-            else {
-
-              archiveArtifacts artifacts: "${rawResultsFile}"
-            }
-            stash "workspace"
-          }
         }
-      }
     }
-  }
 }
