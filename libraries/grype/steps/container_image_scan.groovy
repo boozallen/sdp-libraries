87035c32beef742cf8e3b8ec2f32b3e3f2fd3e20
mackeyaj <mackey_adam@bah.com>
2022-06-14 17:57:59 -0400

    test docker: source

--- libraries/grype/steps/container_image_scan.groovy
+++ libraries/grype/steps/container_image_scan.groovy
@@ -7,53 +7,49 @@ void call() {
         String severityThreshold = config?.fail_on_severity ?: "high"
         List<Exception> errors = []
 
-        //login to container repo
-        login_to_registry{    
-            inside_sdp_image "grype:latest", {
-                unstash "workspace"
-                def images = get_images_to_build()
-                images.each { img ->
-                    String rawResultsFile = "${img.context}-grype-scan-results.json"
-                    //check for grype config file in workspace
-                    if (!fileExists("./${grypeConfig}")) { error "no grype config found" }
-
-                    // perform the grype scan
-                    try {
-                        if (severityThreshold == "none") {
-                            sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} >> ${rawResultsFile}"
-                        }
-                        else {
-                            sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} --fail-on ${severityThreshold} >> ${rawResultsFile}"
-                            echo "No CVE's at or above set threshold!"
-                        }
+        inside_sdp_image "grype:latest", {
+            unstash "workspace"
+            def images = get_images_to_build()
+            images.each { img ->
+                String rawResultsFile = "${img.context}-grype-scan-results.json"
+                //check for grype config file in workspace
+                if (!fileExists("./${grypeConfig}")) { error "no grype config found" }
+                // perform the grype scan
+                try {
+                    if (severityThreshold == "none") {
+                        sh "grype docker:${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} >> ${rawResultsFile}"
                     }
-                    // Catch the error on quality gate failure
-                    catch(Exception err) {
-                        errors.push(err)
-                        //echo "Failed: ${err}"
-                        //echo "Grype Quality Gate Failed. There are one or more CVE's that exceed the maximum allowed severity rating!"
+                    else {
+                        sh "grype docker:${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} --fail-on ${severityThreshold} >> ${rawResultsFile}"
+                        echo "No CVE's at or above set threshold!"
                     }
-                    // display the results in a human-readable format
-                    finally {
-                        if (outputFormat == "json") {
-                            String transformedResultsFile = "${img.context}-grype-scan-results.txt"
-                            def transform_script = resource("transform-grype-scan-results.sh")
-                            writeFile file: "transform-results.sh", text: transform_script
-                            def transformed_results = sh script: "/bin/bash ./transform-results.sh ${rawResultsFile} ${grypeConfig}", returnStdout: true
-                            writeFile file: transformedResultsFile, text: transformed_results.trim()
-                            // archive the results
-                            archiveArtifacts artifacts: "${rawResultsFile}, ${transformedResultsFile}", allowEmptyArchive: true
-                        }
-                        else {
-                            archiveArtifacts artifacts: "${rawResultsFile}", allowEmptyArchive: true
-                        }
+                }
+                // Catch the error on quality gate failure
+                catch(Exception err) {
+                    errors.push(err)
+                    //echo "Failed: ${err}"
+                    //echo "Grype Quality Gate Failed. There are one or more CVE's that exceed the maximum allowed severity rating!"
+                }
+                // display the results in a human-readable format
+                finally {
+                    if (outputFormat == "json") {
+                        String transformedResultsFile = "${img.context}-grype-scan-results.txt"
+                        def transform_script = resource("transform-grype-scan-results.sh")
+                        writeFile file: "transform-results.sh", text: transform_script
+                        def transformed_results = sh script: "/bin/bash ./transform-results.sh ${rawResultsFile} ${grypeConfig}", returnStdout: true
+                        writeFile file: transformedResultsFile, text: transformed_results.trim()
+                        // archive the results
+                        archiveArtifacts artifacts: "${rawResultsFile}, ${transformedResultsFile}", allowEmptyArchive: true
                     }
-                }    
-                stash "workspace"
-                if (!(errors?.empty)) {
-                    errors.each { errs -> 
-                        throw errs
+                    else {
+                        archiveArtifacts artifacts: "${rawResultsFile}", allowEmptyArchive: true
                     }
+                }
+            }    
+            stash "workspace"
+            if (!(errors?.empty)) {
+                errors.each { errs -> 
+                    throw errs
                 }
             }
         }
