5700ac41e801cfa1bd99dde6708b05e1f4bcffcc
mackeyaj <mackey_adam@bah.com>
2022-06-14 08:24:00 -0400

    unit testing grype

---
+++ libraries/grype/test/ContainerImageScanTestSpec.groovy
@@ -0,0 +1,17 @@
+/*
+  Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
+  This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
+*/
+
+package libraries.grype
+
+import JTEPipelineSpecification
+
+
+class ContainerImageScanTestSpec extends JTEPipelineSpecification {
+
+    def ImageScanTest = null
+
+     }
+
+
