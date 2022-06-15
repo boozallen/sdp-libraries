49b5be7879d9903a59215ea377f078ff4309a4f1
mackeyaj <mackey_adam@bah.com>
2022-06-15 08:51:17 -0400

    remove docker version

--- libraries/grype/steps/container_image_scan.groovy
+++ libraries/grype/steps/container_image_scan.groovy
@@ -16,7 +16,6 @@ void call() {
                     //check for grype config file in workspace
                     if (!fileExists("./${grypeConfig}")) { error "no grype config found" }
                     // perform the grype scan
-                    sh "docker --version"
                     try {
                         if (severityThreshold == "none") {
                             sh "grype ${img.registry}/${img.repo}:${img.tag} -o ${outputFormat} >> ${rawResultsFile}"
