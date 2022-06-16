5d1cd983702ed8e1fc4c1c35b38a44b552f25090
mackeyaj <mackey_adam@bah.com>
2022-06-16 16:45:52 -0400

    test contains

--- libraries/grype/steps/container_image_scan.groovy
+++ libraries/grype/steps/container_image_scan.groovy
@@ -12,6 +12,9 @@ void call() {
                 unstash "workspace"
                 def images = get_images_to_build()
                 images.each { img ->
+                    // Use $img.repo to help name our results uniquely. Checks to see if a forward slash exists in the string and remove everything to the left if it does.
+                    println(img.repo.contains(/))
+                    if (img.repo.contains(/))
                     String rawResultsFile = "${img.repo}-grype-scan-results.json"
 //check for grype config file in workspace
 //remove if (!fileExists("./${grypeConfig}")) { error "no grype config found" }
