3fbc945493ff4049600e2ba2538a7db03208d575
mackeyaj <mackey_adam@bah.com>
2022-06-17 17:59:54 -0400

    test

--- libraries/grype/steps/container_image_scan.groovy
+++ libraries/grype/steps/container_image_scan.groovy
@@ -23,7 +23,7 @@ void call() {
                 sh "mkdir test"
                 sh "mkdir test/grype"
                 sh "touch test/grype/config.yaml"
-                sh '''XDG_CONFIG_HOME="/root/workspace/Multi-image-scan_main/test"
+                sh '''export XDG_CONFIG_HOME=/root/workspace/Multi-image-scan_main/test
                 '''
                 sh '''
                     ls -alh 
