b748cf99d5fb811b40c350c6a0f3a298b118a84a
Steven Terrana <steventerrana@gmail.com>
2022-06-29 08:34:31 -0400

    update

--- libraries/grype/library_config.groovy
+++ libraries/grype/library_config.groovy
@@ -1,8 +1,8 @@
 fields{
     optional{
         grype_container = String
-        report_format = String
-        fail_on_severity = String
+        report_format = ["json", "table", "cyclonedx", "template"]
+        fail_on_severity = ["negligible", "low", "medium", "high", "critical"]
         grype_config = String
     }
 }
