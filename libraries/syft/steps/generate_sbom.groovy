c208dbcd9d5b9aa11aa675baa5e1a7fbff8517d2
mackeyaj <mackey_adam@bah.com>
2022-08-17 09:25:24 -0400

    add login_to_registry

--- libraries/syft/steps/generate_sbom.groovy
+++ libraries/syft/steps/generate_sbom.groovy
@@ -14,25 +14,27 @@ void call() {
         //Get list of images to scan (assuming same set built by Docker)
         def images = get_images_to_build()
 
-        images.each { img ->
-            // pull and save images as tarballs
-            String archive_name = "${img.registry}-${img.repo}-${img.tag}.tar".replaceAll("/","-")
-            sh "docker save ${img.registry}/${img.repo}:${img.tag} > ${archive_name}"
-        }
+        login_to_registry {
+            images.each { img ->
+                // pull and save images as tarballs
+                String archive_name = "${img.registry}-${img.repo}-${img.tag}.tar".replaceAll("/","-")
+                sh "docker save ${img.registry}/${img.repo}:${img.tag} > ${archive_name}"
+            }
 
-        stage('Generate SBOM using Syft') {
-            inside_sdp_image "${sbom_container}", {
-                unstash "workspace"
-                images.each { img ->
-                    // perform the syft scan
-                    String archive_name = "${img.registry}-${img.repo}-${img.tag}.tar".replaceAll("/","-")
-                    String results_name = "${img.repo}-${img.tag}-${raw_results_file}".replaceAll("/","-")
-                    sh "syft ${archive_name} -o json > ${results_name}"
+            stage('Generate SBOM using Syft') {
+                inside_sdp_image "${sbom_container}", {
+                    unstash "workspace"
+                    images.each { img ->
+                        // perform the syft scan
+                        String archive_name = "${img.registry}-${img.repo}-${img.tag}.tar".replaceAll("/","-")
+                        String results_name = "${img.repo}-${img.tag}-${raw_results_file}".replaceAll("/","-")
+                        sh "syft ${archive_name} -o json > ${results_name}"
 
-                    // archive the results
-                    archiveArtifacts artifacts: "${results_name}"
+                        // archive the results
+                        archiveArtifacts artifacts: "${results_name}"
+                    }
+                    stash "workspace"
                 }
-                stash "workspace"
             }
         }
     }
