5530e57f9fe7c5d62a7780a7029f3487743acd27
mackeyaj <mackey_adam@bah.com>
2022-09-12 18:01:19 -0400

    vars, start build ARGS

---
+++ libraries/cookiecutter/README.md
@@ -0,0 +1,19 @@
+---
+description: 
+---
+
+# cookiecutter
+
+## Steps
+
+| Step | Description |
+|------|-------------|
+|      |             |
+
+## Configuration
+
+| Library Configuration | Type | Default Value |
+|-----------------------|------|---------------|
+|                       |      |               |
+
+## Dependencies
---
+++ libraries/cookiecutter/steps/scaffold_app.groovy
@@ -0,0 +1,30 @@
+/*
+  Copyright © 2022 Booz Allen Hamilton. All Rights Reserved.
+  This software package is licensed under the Booz Allen Public License.
+  The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
+*/
+
+package libraries.cookiecutter.steps
+
+void call() {
+    stage("Cookiecutter") {
+        String cookiecutterImage = config?.cookiecutter_image ?: "cookiecutter:1.7.3"
+        String templatePath = config?.template_path ?: "./cookiecutter.json"
+        String checkout = config?.checkout ?: null
+        String directory = config?.cookiecutter_json_dir ?: null
+        String outDir = config?.output_directory ?: null
+        String configFile = config?.config_file ?: null
+        String defaultConf = config?.default_config ?: null
+        String debugFile = config?.debug_file ?: null
+        String ARGS = ""
+        Boolean replay = config?.replay ?: false
+        Boolean noInput = config?.no_input ?: false
+        Boolean overwrite = config?.overwrite_if_exists ?: false
+        Boolean skip = config?.skip_if_file_exists ?: false
+        Boolean showVer = config?.show_version ?: false
+        Boolean debugOn = config?.verbose ?: false
+
+        if ()
+
+    }
+}
