32f2ace2549e7314a71e18de6185dbbc490512c9
mackeyaj <mackey_adam@bah.com>
2022-09-20 16:32:37 -0400

    add scm capability

--- libraries/cookiecutter/steps/scaffold_app.groovy
+++ libraries/cookiecutter/steps/scaffold_app.groovy
@@ -7,62 +7,86 @@
 package libraries.cookiecutter.steps
 
 void call() {
-    stage("Cookiecutter") {
-        String cookiecutterImage = config?.cookiecutter_image ?: "cookiecutter:2.1.1"
-        String templatePath = config?.template_path ?: null //TEMPLATE
-        String checkout = config?.checkout ?: null //OPTION
-        String scmPull = config?.scm_url ?: null //TEMPLATE
-        String outDir = config?.output_directory ?: null //OPTION
-        String cookieCutterJson = config?.cookie_cutter_json ?: null //Overwrite cookiecutter.json with this file
-        String cookieCutterFolder = config?.cookie_cutter_folder ?: null
-        String ARGS = "" //Final Command
-        Boolean noInput = config?.no_input ?: false //OPTION
-        Boolean debugOn = config?.verbose ?: false //OPTION
-        Boolean overwriteWorkspace = config?.overwrite_workspace ?: false
-        Boolean shouldFail = false
+  stage("Cookiecutter") {
+    String cookiecutterImage = config?.cookiecutter_image ?: "cookiecutter:2.1.1"
+    String templatePath = config?.template_path ?: null //TEMPLATE
+    String scmPull = config?.scm_url ?: null //TEMPLATE
+    String scmCred = config?.scm_cred ?: null //creds to pull cc template from scm
+    String checkout = config?.checkout ?: null //OPTION
+    String cookiecutterDir = config?.cookiecutterDirectory ?: null //OPTION
+    String outDir = config?.output_directory ?: null //OPTION
+    String cookieCutterJson = config?.cookie_cutter_json ?: null //Overwrite cookiecutter.json with this file
+    String projectFolder = config?.project_folder ?: null
+    String ARGS = "" //Final Command
+    Boolean noInput = config?.no_input ?: true //OPTION
+    Boolean debugOn = config?.verbose ?: false //OPTION
+    Boolean overwriteWorkspace = config?.overwrite_workspace ?: false
+    Boolean shouldFail = false
         
-        if (outDir) {
-          ARGS += "--output-dir ${outDir} "
-        }
-
-        if (noInput) {
-          ARGS += "--no-input "
-        }
-        
-        if (debugOn) {
-          ARGS += "--verbose "
-        }
-
-        
-        //cookiecutter [OPTIONS] [TEMPLATE] [EXTRA_CONTEXT]...
-        inside_sdp_image(cookiecutterImage) {
-            if (templatePath) {
-                sh 'ls -alh' //remove
-                unstash 'workspace'
-                sh 'ls -alh' //remove
-            
-                ARGS += templatePath
-
-                if (cookieCutterJson) {
-                    sh "cp -f ${cookieCutterJson} ./cookiecutter.json"
-                    sh 'ls -alh' //remove
-                }
-
-                try {
-                    sh "cookiecutter ${ARGS}"
-                }
-                catch (Exception err) {
-                    shouldFail = true
-                    echo "Failed: {$err}"
-                }
-                finally {
-                    if (overwriteWorkspace) {
-                        dir("scaffold_pipeline") {
-                        stash name: 'workspace', allowEmpty: true, useDefaultExcludes: false
-                        }
-                    }
-                }
-            }
-        }
+    if (checkout) {
+        ARGS += "--checkout ${checkout} "
     }
+
+    if (cookiecutterDir) {
+      ARGS += "--directory ${cookiecutterDir}"
+    }
+    
+    if (outDir) {
+        ARGS += "--output-dir ${outDir} "
+    }
+
+    if (noInput) {
+      ARGS += "--no-input "
+    }
+        
+    if (debugOn) {
+      ARGS += "--verbose "
+    }
+
+        
+    //cookiecutter [OPTIONS] [TEMPLATE] [EXTRA_CONTEXT]...
+    inside_sdp_image(cookiecutterImage) {
+      if (templatePath) {
+        unstash 'workspace'
+            
+        ARGS += templatePath
+
+        if (cookieCutterJson) {
+          sh "cp -f ${cookieCutterJson} ./cookiecutter.json"
+          sh 'ls -alh' //remove
+        }
+      }
+      else if (scmPull) {
+        if (scmCred) {
+          withCredentials([string(credentialsId: ${scmCred}, variable: 'PAT')]) {
+            scmPull.replaceFirst("://","://${PAT}@")
+            ARGS += scmPull
+          }
+        }  
+        else {
+          ARGS += scmPull
+        }
+      
+        try {
+          sh "cookiecutter ${ARGS}"
+        }
+        catch (Exception err) {
+          shouldFail = true
+          echo "Failed: {$err}"
+        }
+        finally {
+          if (overwriteWorkspace) {
+            if (outDir) {
+              dir(${outDir} + ${projectFolder})
+            }
+            else {
+              dir(${projectFolder}) {
+                stash name: 'workspace', allowEmpty: true, useDefaultExcludes: false
+              }
+            }
+          }
+        }
+      } 
+    }
+  }
 }
