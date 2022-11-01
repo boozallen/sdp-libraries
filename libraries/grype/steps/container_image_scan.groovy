389fd254d3152a3ebc5a8ab099b4d736a0d6f4a6
mackeyaj <mackey_adam@bah.com>
2022-11-01 16:54:04 -0400

    rm String baseDir

--- libraries/grype/steps/container_image_scan.groovy
+++ libraries/grype/steps/container_image_scan.groovy
@@ -7,7 +7,6 @@ void call() {
         String severityThreshold = config?.fail_on_severity ?: 'high'
         String grypeConfig = config?.grype_config
         Boolean scanSbom = config?.scan_sbom ?: false
-        String baseDir = "./"
         String resultsFileFormat = ".txt"
         String ARGS = ""
         // is flipped to True if an image scan fails
