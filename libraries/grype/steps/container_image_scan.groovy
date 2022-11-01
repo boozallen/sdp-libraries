d89aad37f18417cda9c477c03f3efecbc69c3746
mackeyaj <mackey_adam@bah.com>
2022-11-01 13:48:27 -0400

    test sbom_scan var

--- libraries/grype/steps/container_image_scan.groovy
+++ libraries/grype/steps/container_image_scan.groovy
@@ -6,14 +6,12 @@ void call() {
         String outputFormat = config?.report_format ?: 'json'
         String severityThreshold = config?.fail_on_severity ?: 'high'
         String grypeConfig = config?.grype_config
-        //Boolean scanSbom = config?.grype.scan_sbom ?: false
+        Boolean scanSbom = config?.grype.scan_sbom ?: false
         String resultsFileFormat = ".txt"
         String ARGS = ""
         // is flipped to True if an image scan fails
         Boolean shouldFail = false 
-        //test
-        println(config.syft.raw_results_file)
-        //end
+
         if (outputFormat != null) {
             ARGS += "-o ${outputFormat} "
             if (outputFormat == 'json') {
