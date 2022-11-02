58616020a41b02375b154a33b8fbf7c54318f849
mackeyaj <97459289+mackeyaj@users.noreply.github.com>
2022-11-02 13:38:30 -0400

    Syft multiple formats (#189)
    * push syft multi format
    
    * test for loop
    
    * test multi format syft
    
    * check for report output
    
    * fix increment operator
    
    * fix sbom_format data type
    
    * remove sbom_format default
    
    * test sbom_format.size()
    
    * ts for loop
    
    * testing loop
    
    * fix equal operator ==
    
    * testing loop
    
    * check args
    
    * ts list
    
    * ts ARGS
    
    * remove size()
    
    * loop troubleshoot
    
    * Test using ArrayList
    
    * comment out loops
    
    * print loop index
    
    * test with string concatenation
    
    * rm println
    
    * test toString
    
    * test arraylist
    
    * test
    
    * test
    
    * test ARG building
    
    * testing
    
    * test w/o tostring
    
    * test
    
    * move sbom_format to LinkedHashMap for extensions
    
    * check sbom_format
    
    * test
    
    * fix lib conf
    
    * revert to ArrayList for sbom_format var
    
    * fixed lib config
    
    * rm ! from index identifier
    
    * fix format
    
    * test
    
    * ts stdout
    
    * comment out erring cmd
    
    * missed end "
    
    * rm extra text
    
    * test --output
    
    * check ARGS
    
    * test
    
    * add space for multi outputs
    
    * add formatter var
    
    * ts formatter
    
    * add space
    
    * cmd formatting
    
    * archival ts
    
    * test archival
    
    * trim trailing comma
    
    * escape the $
    
    * test if statements
    
    * fixes
    
    * add exception handling
    
    * test exception
    
    * skip archival if failed
    
    * echo exception
    
    * test
    
    * test err
    
    * test echo err
    
    * test error
    
    * exception test
    
    * test throw err
    
    * test error
    
    * test
    
    * test error
    
    * add shouldFail bool
    
    * Syft Unit test changes
    
    * push syft Unit tests
    
    * update syft docs
    
    * Fixed artifacts archival
    
    * rm unused execption var
    
    * updates README file

--- libraries/syft/steps/generate_sbom.groovy
+++ libraries/syft/steps/generate_sbom.groovy
@@ -8,8 +8,11 @@ package libraries.syft.steps
 void call() {
     stage('Generate SBOM using Syft') {
         //Import settings from config
-        String raw_results_file = config?.raw_results_file ?: 'syft-sbom-results.json'
+        String raw_results_file = config?.raw_results_file ?: 'syft-sbom-results' // leave off file extension so that it can be added based off off selected formats
         String sbom_container = config?.sbom_container ?: 'syft:0.47.0'
+        ArrayList sbom_format = config?.sbom_format ?: ["json"]
+        String artifacts = ""
+        boolean shouldFail = false
 
         //Get list of images to scan (assuming same set built by Docker)
         def images = get_images_to_build()
@@ -17,12 +20,40 @@ void call() {
             login_to_registry {
                 unstash "workspace"
                 images.each { img ->
-                    // perform the syft scan
+                    String ARGS = "-q"
                     String results_name = "${img.repo}-${img.tag}-${raw_results_file}".replaceAll("/","-")
-                    sh "syft ${img.registry}/${img.repo}:${img.tag} -o json > ${results_name}"
+                      sbom_format.each { format ->
+                        String formatter = ""
+                        if(format == "json" || format == "cyclonedx-json" || format == "spdx-json" || format == "github") {
+                          formatter += "${results_name}-${format}.json"
+                        }
+                        else if(format == "text" || format == "spdx-tag-value" || format == "table") {
+                          formatter += "${results_name}-${format}.txt"
+                        }
+                        else if (format == "cyclonedx-xml") {
+                          formatter += "${results_name}-${format}.xml"
+                        }
 
-                    // archive the results
-                    archiveArtifacts artifacts: "${results_name}"
+                        ARGS += " -o ${format}=${formatter} "
+                        artifacts += "${formatter},"
+                      }
+
+                    // perform the syft scan
+                    try {
+                      sh "syft ${img.registry}/${img.repo}:${img.tag} ${ARGS}"
+                    }
+                    catch(Exception err) {
+                      shouldFail = true
+                      echo "SBOM generation Failed: ${err}"  
+                    }
+                    finally {
+                      if(shouldFail){
+                        error("SBOM Stage Failed")
+                      }
+                      else {
+                        archiveArtifacts artifacts: "${artifacts.replaceAll(',$', "")}"
+                      }
+                    }
                 }
                 stash "workspace"
             }
