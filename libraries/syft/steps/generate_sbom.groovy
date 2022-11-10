4ea83e91d01f9116e4b3e878852dc244bf06c512
mackeyaj <mackey_adam@bah.com>
2022-11-10 10:59:31 -0500

    format and trailing whitespace

--- libraries/syft/steps/generate_sbom.groovy
+++ libraries/syft/steps/generate_sbom.groovy
@@ -6,57 +6,58 @@
 package libraries.syft.steps
 
 void call() {
-    stage('Generate SBOM using Syft') {
-        //Import settings from config
-        String raw_results_file = config?.raw_results_file ?: 'syft-sbom-results' // leave off file extension so that it can be added based off off selected formats
-        String sbom_container = config?.sbom_container ?: 'syft:0.47.0'
-        ArrayList sbom_format = config?.sbom_format ?: ["json"]
-        String artifacts = ""
-        boolean shouldFail = false
+  stage('Generate SBOM using Syft') {
+    //Import settings from config
+    String raw_results_file = config?.raw_results_file ?: 'syft-sbom-results' // leave off file extension so that it can be added based off off selected formats
+    String sbom_container = config?.sbom_container ?: 'syft:0.47.0'
+    ArrayList sbom_format = config?.sbom_format ?: ["json"]
+    String artifacts = ""
+    boolean shouldFail = false
 
-        //Get list of images to scan (assuming same set built by Docker)
-        def images = get_images_to_build()
-        inside_sdp_image "${sbom_container}", {
-            login_to_registry {
-                unstash "workspace"
-                images.each { img ->
-                    String ARGS = "-q"
-                    String results_name = "${img.repo}-${img.tag}-${raw_results_file}".replaceAll("/","-")
-                      sbom_format.each { format ->
-                        String formatter = ""
-                        if(format == "json" || format == "cyclonedx-json" || format == "spdx-json" || format == "github") {
-                          formatter += "${results_name}-${format}.json"
-                        }
-                        else if(format == "text" || format == "spdx-tag-value" || format == "table") {
-                          formatter += "${results_name}-${format}.txt"
-                        }
-                        else if (format == "cyclonedx-xml") {
-                          formatter += "${results_name}-${format}.xml"
-                        }
-
-                        ARGS += " -o ${format}=${formatter} "
-                        artifacts += "${formatter},"
-                      }
-
-                    // perform the syft scan
-                    try {
-                      sh "syft ${img.registry}/${img.repo}:${img.tag} ${ARGS}"
-                    }
-                    catch(Exception err) {
-                      shouldFail = true
-                      echo "SBOM generation Failed: ${err}"  
-                    }
-                    finally {
-                      if(shouldFail){
-                        error("SBOM Stage Failed")
-                      }
-                      else {
-                        archiveArtifacts artifacts: "${artifacts.replaceAll(',$', "")}"
-                      }
-                    }
-                }
-                stash "workspace"
+    //Get list of images to scan (assuming same set built by Docker)
+    def images = get_images_to_build()
+    inside_sdp_image "${sbom_container}", {
+      login_to_registry {
+        unstash "workspace"
+        images.each { img ->
+          String ARGS = "-q"
+          String results_name = "${img.repo}-${img.tag}-${raw_results_file}".replaceAll("/","-")
+          sbom_format.each { format ->
+            String formatter = ""
+            if(format == "json" || format == "cyclonedx-json" || format == "spdx-json" || format == "github") {
+              formatter += "${results_name}-${format}.json"
             }
+            else if(format == "text" || format == "spdx-tag-value" || format == "table") {
+              formatter += "${results_name}-${format}.txt"
+            }
+            else if (format == "cyclonedx-xml") {
+              formatter += "${results_name}-${format}.xml"
+            }
+
+            ARGS += " -o ${format}=${formatter} "
+            artifacts += "${formatter},"
+          }
+
+          // perform the syft scan
+          try {
+            sh "syft ${img.registry}/${img.repo}:${img.tag} ${ARGS}"
+          }
+          catch(Exception err) {
+            shouldFail = true
+            echo "SBOM generation Failed: ${err}"
+          }
+          finally {
+            if(shouldFail){
+              error("SBOM Stage Failed")
+            }
+            else {
+              archiveArtifacts artifacts: "${artifacts.replaceAll(',$', "")}"
+            }
+          }
         }
+
+        stash "workspace"
+      }
     }
+  }
 }
