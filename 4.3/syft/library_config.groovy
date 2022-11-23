de88035b75a406ef6f9b57c86866cad8a1b402ae
docs deployer <null@null.com>
2022-11-23 19:08:40 +0000

    Deployed 20fcaa1 to 4.3 with MkDocs 1.2.3 and mike 1.1.2

--- 4.3/syft/library_config.groovy
+++ 4.3/syft/library_config.groovy
@@ -5,5 +5,7 @@ fields {
         raw_results_file = String
         sbom_container = String
         sbom_format = ArrayList
+        remove_syft_config = Boolean
+        config_name = String
     }
 }
